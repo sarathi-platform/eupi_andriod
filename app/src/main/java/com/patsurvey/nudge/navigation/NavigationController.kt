2e5657b5009797b8bc1ef4c8190e1a4830771fc1
Puneet Sharma <puneet.sharma@tothenew.com>
2023-05-03 16:06:11 +0530

    confict resolved

--- app/src/main/java/com/patsurvey/nudge/navigation/NavigationController.kt
+++ app/src/main/java/com/patsurvey/nudge/navigation/NavigationController.kt
@@ -162,12 +162,6 @@ fun HomeScreenFlowNavigation(
                     .then(modifier)
             )
         }
-        composable(route = ScreenRoutes.DIDI_SCREEN.route) {
-            DidiScreen(modifier = Modifier.then(modifier))
-        }
-        composable(route = ScreenRoutes.MORE_SCREEN.route) {
-            MoreScreen(modifier = Modifier.then(modifier))
-        }
         composable(route = ScreenRoutes.VILLAGE_SELECTION_SCREEN.route) {
             VillageSelectionScreen(navController = homeScreenNavController, viewModel = hiltViewModel(), modifier = Modifier.fillMaxSize())
         }
