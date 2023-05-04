6683dfde3edb2f3285fdccb75e09daa323ac6bc8
anupambhardwaj <anupam.bhardwaj@siriusxm.com>
2023-05-04 14:36:59 +0530

    Handled Exception if language api fails then a default language (English) should be added.

--- app/src/main/java/com/patsurvey/nudge/activities/ui/splash/ConfigViewModel.kt
+++ app/src/main/java/com/patsurvey/nudge/activities/ui/splash/ConfigViewModel.kt
@@ -1,10 +1,12 @@
 package com.patsurvey.nudge.activities.ui.splash
 
+import android.util.Log
 import com.patsurvey.nudge.base.BaseViewModel
 import com.patsurvey.nudge.data.prefs.PrefRepo
 import com.patsurvey.nudge.database.LanguageEntity
 import com.patsurvey.nudge.database.dao.LanguageListDao
 import com.patsurvey.nudge.network.interfaces.ApiService
+import com.patsurvey.nudge.utils.FAIL
 import com.patsurvey.nudge.utils.SPLASH_SCREEN_DURATION
 import com.patsurvey.nudge.utils.SUCCESS
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -22,33 +24,59 @@ class ConfigViewModel @Inject constructor(
         return prefRepo.getAccessToken()?.isNotEmpty() == true
     }
 
-    init {
-
-    }
-
     fun fetchLanguageDetails(callBack: () -> Unit) {
-        job = CoroutineScope(Dispatchers.IO + exceptionHandler).launch {
-            if (isLoggedIn()) {
-                delay(SPLASH_SCREEN_DURATION)
-                withContext(Dispatchers.Main) {
-                    callBack()
-                }
-            }else {
-                val response = apiInterface.configDetails()
-                withContext(Dispatchers.IO) {
-                    if (response.status.equals(SUCCESS, true)) {
-                        response.data?.let {
-                            languageListDao.insertAll(it.languageList)
-                            delay(SPLASH_SCREEN_DURATION)
+        job = CoroutineScope(Dispatchers.IO).launch {
+            try {
+                if (isLoggedIn()) {
+                    delay(SPLASH_SCREEN_DURATION)
+                    withContext(Dispatchers.Main) {
+                        callBack()
+                    }
+                } else {
+                    val response = apiInterface.configDetails()
+                    withContext(Dispatchers.IO) {
+                        if (response.status.equals(SUCCESS, true)) {
+                            response.data?.let {
+                                languageListDao.insertAll(it.languageList)
+                                delay(SPLASH_SCREEN_DURATION)
+                                withContext(Dispatchers.Main) {
+                                    callBack()
+                                }
+                            }
+                        } else if (response.status.equals(FAIL, true)) {
+                            addDefaultLanguage()
+                            withContext(Dispatchers.Main) {
+                                callBack()
+                            }
+                        } else {
+                            onError("Error : ${response.message} ")
+                            addDefaultLanguage()
                             withContext(Dispatchers.Main) {
                                 callBack()
                             }
                         }
-                    } else {
-                        onError("Error : ${response.message} ")
                     }
+                }
+            } catch (ex: Exception) {
+                onError("Error : ${ex.localizedMessage}")
+                addDefaultLanguage()
+                withContext(Dispatchers.Main) {
+                    callBack()
                 }
             }
         }
     }
+
+    fun addDefaultLanguage() {
+        languageListDao.insertLanguage(
+            LanguageEntity(
+                id = 1,
+                language = "English",
+                langCode = "en",
+                orderNumber = 1,
+                localName = "English"
+            )
+        )
+    }
+
 }
