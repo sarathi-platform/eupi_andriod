a3c4ae21a52e1ccdc263cd7597d2c8660ad374fe
anupambhardwaj <anupam.bhardwaj@siriusxm.com>
2023-05-08 15:18:42 +0530

    Updated Data type for Latitude and Longitudes.

--- app/src/main/java/com/patsurvey/nudge/utils/LocationCoordinates.kt
+++ app/src/main/java/com/patsurvey/nudge/utils/LocationCoordinates.kt
@@ -1,6 +1,6 @@
 package com.patsurvey.nudge.utils
 
 data class LocationCoordinates(
-    val lat: Int? = null,
-    val long: Int? = null
+    val lat: Double? = null,
+    val long: Double? = null
 )
