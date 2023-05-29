593061597449a8b1c41d48ee911f8b20151a86c0
anupambhardwaj <anupam.bhardwaj@siriusxm.com>
2023-05-29 17:27:25 +0530

    Added code for PDF generation, downloading and share functionality.

--- app/src/main/java/com/patsurvey/nudge/activities/ui/digital_forms/DigitalFormAViewModel.kt
+++ app/src/main/java/com/patsurvey/nudge/activities/ui/digital_forms/DigitalFormAViewModel.kt
@@ -1,25 +1,13 @@
 package com.patsurvey.nudge.activities.ui.digital_forms
 
 import android.content.Context
-import android.graphics.pdf.PdfDocument
-import android.os.Environment
-import android.print.PrintAttributes
-import android.text.Layout
-import androidx.compose.ui.text.font.Typeface
-import androidx.lifecycle.viewModelScope
 import com.patsurvey.nudge.base.BaseViewModel
 import com.patsurvey.nudge.data.prefs.PrefRepo
 import com.patsurvey.nudge.database.DidiEntity
 import com.patsurvey.nudge.database.dao.DidiDao
-import com.patsurvey.nudge.model.dataModel.DidiDetailsModel
-import com.wwdablu.soumya.simplypdf.SimplyPdf
-import com.wwdablu.soumya.simplypdf.composers.properties.TableProperties
-import com.wwdablu.soumya.simplypdf.composers.properties.TextProperties
-import com.wwdablu.soumya.simplypdf.composers.properties.cell.Cell
-import com.wwdablu.soumya.simplypdf.composers.properties.cell.TextCell
-import com.wwdablu.soumya.simplypdf.document.DocumentInfo
-import com.wwdablu.soumya.simplypdf.document.Margin
 import com.patsurvey.nudge.network.model.ErrorModel
+import com.patsurvey.nudge.utils.PREF_WEALTH_RANKING_COMPLETION_DATE
+import com.patsurvey.nudge.utils.PdfUtils
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -27,8 +15,6 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import java.io.File
-import java.util.LinkedList
 import javax.inject.Inject
 
 @HiltViewModel
@@ -51,109 +37,9 @@ class DigitalFormAViewModel @Inject constructor(
         /*TODO("Not yet implemented")*/
     }
 
-    suspend fun pdf(context: Context){
-        val margins:Int = 16
-        val simplyPdfDocument = SimplyPdf.with(context, File("${Environment.getExternalStorageDirectory().absoluteFile}/Digital Form A.pdf"))
-            .colorMode(DocumentInfo.ColorMode.COLOR)
-            .paperSize(PrintAttributes.MediaSize.ISO_A4)
-            .margin(Margin(margins.toUInt(), margins.toUInt(), margins.toUInt(), margins.toUInt()))
-            .paperOrientation(DocumentInfo.Orientation.PORTRAIT)
-            .build()
-
-        simplyPdfDocument.text.write("Digital Form A", TextProperties().apply {
-            textSize = 13
-            textColor = "#000000"
-            typeface = android.graphics.Typeface.DEFAULT_BOLD
-            alignment = Layout.Alignment.ALIGN_CENTER
-
-        })
-        simplyPdfDocument.text.write("List of households categorized into Rich, Medium, and Poor in Participatory Wealth Ranking",
-            TextProperties().apply {
-                textSize = 10
-                textColor = "#000000"
-                typeface = android.graphics.Typeface.DEFAULT
-                alignment = Layout.Alignment.ALIGN_CENTER
-
-            })
-
-        val headerRow = LinkedList<Cell>().apply {
-                add(TextCell("S. No.\n" +
-                        "(1)", TextProperties().apply {
-                    textSize = 10
-                    textColor = "#000000"
-                    typeface = android.graphics.Typeface.DEFAULT
-                    alignment = Layout.Alignment.ALIGN_CENTER
-                }, Cell.MATCH_PARENT))
-                add(TextCell("HH No.\n" +
-                        "(2)", TextProperties().apply {
-                    textSize = 10
-                    textColor = "#000000"
-                    typeface = android.graphics.Typeface.DEFAULT
-                    alignment = Layout.Alignment.ALIGN_CENTER
-                }, Cell.MATCH_PARENT))
-                add(TextCell("Name of the Didi\n" +
-                        "(3)", TextProperties().apply {
-                    textSize = 10
-                    textColor = "#000000"
-                    typeface = android.graphics.Typeface.DEFAULT
-                    alignment = Layout.Alignment.ALIGN_CENTER
-                }, Cell.MATCH_PARENT))
-                add(TextCell("Name of Husband/ Father\n" +
-                        "(4)", TextProperties().apply {
-                    textSize = 10
-                    textColor = "#000000"
-                    typeface = android.graphics.Typeface.DEFAULT
-                    alignment = Layout.Alignment.ALIGN_CENTER
-                }, Cell.MATCH_PARENT))
-                add(TextCell("Name of \n" +
-                        "the Hamlet/Tola/Para\n" +
-                        "(5)", TextProperties().apply {
-                    textSize = 10
-                    textColor = "#000000"
-                    typeface = android.graphics.Typeface.DEFAULT
-                    alignment = Layout.Alignment.ALIGN_CENTER
-                }, Cell.MATCH_PARENT))
-                add(TextCell("Social Category\n" +
-                        "(6)", TextProperties().apply {
-                    textSize = 10
-                    textColor = "#000000"
-                    typeface = android.graphics.Typeface.DEFAULT
-                    alignment = Layout.Alignment.ALIGN_CENTER
-                }, Cell.MATCH_PARENT))
-                add(TextCell("Categorized as (Rich, \n" +
-                        "Medium, Poor)\n" +
-                        "(7)", TextProperties().apply {
-                    textSize = 10
-                    textColor = "#000000"
-                    typeface = android.graphics.Typeface.DEFAULT
-                    alignment = Layout.Alignment.ALIGN_CENTER
-                }, Cell.MATCH_PARENT))
-            }
-
-        val rows = LinkedList<LinkedList<Cell>>().apply {
-            add(headerRow)
-
-//            didiDetailList.value.forEach {
-//                add(
-//                    LinkedList<Cell>().apply {
-//
-//                    }
-//                )
-//            }
-        }
-
-        simplyPdfDocument.table.draw(rows, TableProperties().apply {
-            borderColor = "#000000"
-            borderWidth = 1
-            drawBorder = true
-        })
-
-        simplyPdfDocument.finish()
-    }
-
-    fun generatePDF(context: Context) {
+    fun generateFormAPDF(context: Context, callBack: () -> Unit) {
         job = CoroutineScope(Dispatchers.IO + exceptionHandler).launch {
-            pdf(context)
+            PdfUtils.getFormAPdf(context = context, villageEntity = prefRepo.getSelectedVillage(), didiDetailList = didiDetailList.value, prefRepo.getPref(PREF_WEALTH_RANKING_COMPLETION_DATE, "") ?: "")
         }
     }
 
---
+++ app/src/main/java/com/patsurvey/nudge/utils/PdfUtils.kt
@@ -0,0 +1,370 @@
+package com.patsurvey.nudge.utils
+
+import android.content.Context
+import android.graphics.Typeface
+import android.os.Environment
+import android.print.PrintAttributes
+import android.text.Layout
+import com.patsurvey.nudge.database.DidiEntity
+import com.patsurvey.nudge.database.VillageEntity
+import com.wwdablu.soumya.simplypdf.SimplyPdf
+import com.wwdablu.soumya.simplypdf.SimplyPdfDocument
+import com.wwdablu.soumya.simplypdf.composers.properties.TableProperties
+import com.wwdablu.soumya.simplypdf.composers.properties.TextProperties
+import com.wwdablu.soumya.simplypdf.composers.properties.cell.Cell
+import com.wwdablu.soumya.simplypdf.composers.properties.cell.TextCell
+import com.wwdablu.soumya.simplypdf.document.DocumentInfo
+import com.wwdablu.soumya.simplypdf.document.Margin
+import java.io.File
+import java.util.*
+
+object PdfUtils {
+
+    private const val SR_NO_HEADING_TEXT = "S. No.\n" + "(1)"
+    private const val HOUSE_NO_HEADING_TEXT = "HH No.\n" + "(2)"
+    private const val DIDI_NAME_HEADING_TEXT = "Name of the Didi\n" + "(3)"
+    private const val GUARDIAN_NAME_HEADING_TEXT = "Name of Husband/ Father\n" + "(4)"
+    private const val TOLA_NAME_HEADING_TEXT = "Name of \n" + "the Hamlet/Tola/Para\n" + "(5)"
+
+    private val margins: UInt = 16u
+    private const val serialNumberCellWidth = 50
+    private const val dataCellWidth = 125
+    private val titleTextProperties = getTitleTextProperties()
+    private val subTitleTextProperties = getSubTitleTextProperties()
+    private val contentTextProperties = getContentTextProperties()
+    private val cellDataTextProperties = getTextPropertiesForCell()
+
+    suspend fun getFormAPdf(context: Context, villageEntity: VillageEntity, didiDetailList: List<DidiEntity>, completionDate: String){
+
+        val simplyPdfDocument = getSimplePdfDocument(context, villageEntity, "digital_form_a")
+
+        simplyPdfDocument.text.write("Digital Form A", titleTextProperties)
+        simplyPdfDocument.text.write("List of households categorized into Rich, Medium, and Poor in Participatory Wealth Ranking", subTitleTextProperties)
+
+        simplyPdfDocument.text.write("Village Name: ${villageEntity.name} \t VO Name: ${villageEntity.federationName}",
+            contentTextProperties)
+        simplyPdfDocument.text.write("Date of CRP Drive: $completionDate",
+            contentTextProperties)
+
+        simplyPdfDocument.insertEmptyLines(1)
+
+        val headerRow = LinkedList<Cell>().apply {
+            add(TextCell(SR_NO_HEADING_TEXT, cellDataTextProperties, serialNumberCellWidth))
+            add(TextCell(HOUSE_NO_HEADING_TEXT, cellDataTextProperties, dataCellWidth))
+            add(
+                TextCell(DIDI_NAME_HEADING_TEXT, cellDataTextProperties, dataCellWidth)
+            )
+            add(
+                TextCell(GUARDIAN_NAME_HEADING_TEXT, cellDataTextProperties, dataCellWidth)
+            )
+            add(
+                TextCell(TOLA_NAME_HEADING_TEXT, cellDataTextProperties, dataCellWidth)
+            )
+            add(
+                TextCell("Social Category\n" +
+                    "(6)", cellDataTextProperties, dataCellWidth)
+            )
+            add(
+                TextCell("Categorized as (Rich, \n" +
+                    "Medium, Poor)\n" +
+                    "(7)", cellDataTextProperties, dataCellWidth)
+            )
+        }
+
+        val rows = LinkedList<LinkedList<Cell>>().apply {
+            add(headerRow)
+
+            didiDetailList.forEachIndexed { index, didiEntity ->
+                add(
+                    LinkedList<Cell>().apply {
+                        add(TextCell("${index + 1}", cellDataTextProperties, serialNumberCellWidth))
+                        add(
+                            TextCell("${didiEntity.address}", cellDataTextProperties, dataCellWidth)
+                        )
+                        add(
+                            TextCell("${didiEntity.name}", cellDataTextProperties, dataCellWidth)
+                        )
+                        add(
+                            TextCell("${didiEntity.guardianName}", cellDataTextProperties, dataCellWidth)
+                        )
+                        add(
+                            TextCell("${didiEntity.cohortName}", cellDataTextProperties, dataCellWidth)
+                        )
+                        add(
+                            TextCell("${didiEntity.castName}", cellDataTextProperties, dataCellWidth))
+                        add(
+                            TextCell("${didiEntity.wealth_ranking}", cellDataTextProperties, dataCellWidth))
+                    }
+                )
+            }
+        }
+
+        simplyPdfDocument.table.draw(rows, TableProperties().apply {
+            borderColor = "#000000"
+            borderWidth = 1
+            drawBorder = true
+        })
+
+        simplyPdfDocument.insertEmptyLines(1)
+
+        simplyPdfDocument.text.write("Findings:", TextProperties().apply {
+            textSize = 8
+            textColor = "#000000"
+            typeface = Typeface.DEFAULT_BOLD
+            alignment = Layout.Alignment.ALIGN_NORMAL
+
+        })
+
+        simplyPdfDocument.text.write("Total no. of families in the village: ${didiDetailList.size}",
+            TextProperties().apply {
+            textSize = 8
+            textColor = "#000000"
+            typeface = Typeface.DEFAULT
+            bulletSymbol = "1"
+            alignment = Layout.Alignment.ALIGN_NORMAL
+
+        })
+
+        simplyPdfDocument.text.write("Total no. of families categorized as Poor: ${didiDetailList.filter { it.wealth_ranking == WealthRank.POOR.rank }.size}",
+            TextProperties().apply {
+            textSize = 8
+            textColor = "#000000"
+            typeface = Typeface.DEFAULT
+            bulletSymbol = "2"
+            alignment = Layout.Alignment.ALIGN_NORMAL
+
+        })
+
+        simplyPdfDocument.text.write("Total no. of families categorized as Medium: ${didiDetailList.filter { it.wealth_ranking == WealthRank.MEDIUM.rank }.size}",
+            TextProperties().apply {
+            textSize = 8
+            textColor = "#000000"
+            typeface = Typeface.DEFAULT
+            bulletSymbol = "3"
+            alignment = Layout.Alignment.ALIGN_NORMAL
+
+        })
+
+        simplyPdfDocument.text.write("Total no. of families categorized as Rich: ${didiDetailList.filter { it.wealth_ranking == WealthRank.RICH.rank }.size}",
+            TextProperties().apply {
+            textSize = 8
+            textColor = "#000000"
+            typeface = Typeface.DEFAULT
+            bulletSymbol = "4"
+            alignment = Layout.Alignment.ALIGN_NORMAL
+
+        })
+
+        simplyPdfDocument.finish()
+    }
+
+
+
+    suspend fun getFormBPdf(context: Context, villageEntity: VillageEntity, didiDetailList: List<DidiEntity>, completionDate: String) {
+
+        val simplyPdfDocument = getSimplePdfDocument(context, villageEntity, "digital_form_b")
+
+        simplyPdfDocument.text.write("Digital Form B", titleTextProperties)
+
+        simplyPdfDocument.text.write("Families tentatively selected as Ultra-Poor after mobile-based PAT Survey", subTitleTextProperties)
+
+        simplyPdfDocument.text.write("Village Name: ${villageEntity.name} \t VO Name: ${villageEntity.federationName}", contentTextProperties)
+
+        simplyPdfDocument.text.write("Date of CRP Drive: $completionDate", contentTextProperties)
+
+        simplyPdfDocument.text.write("Total no. of Ultra-Poor families selected by CRP: ${didiDetailList.size}", contentTextProperties)
+
+        simplyPdfDocument.insertEmptyLines(1)
+
+        val headerRow = LinkedList<Cell>().apply {
+            add(TextCell(SR_NO_HEADING_TEXT, cellDataTextProperties, serialNumberCellWidth))
+            add(TextCell(HOUSE_NO_HEADING_TEXT, cellDataTextProperties, dataCellWidth))
+            add(
+                TextCell(DIDI_NAME_HEADING_TEXT, cellDataTextProperties, dataCellWidth)
+            )
+            add(
+                TextCell(GUARDIAN_NAME_HEADING_TEXT, cellDataTextProperties, dataCellWidth)
+            )
+            add(
+                TextCell(TOLA_NAME_HEADING_TEXT, cellDataTextProperties, dataCellWidth)
+            )
+            add(
+                TextCell("Caste (ST/PVTG/SC/OBC/General)\n" +
+                        "(6)", cellDataTextProperties, dataCellWidth)
+            )
+            add(
+                TextCell("SHG member (Yes/ No)\n" +
+                        "(7)", cellDataTextProperties, dataCellWidth)
+            )
+        }
+
+        val rows = LinkedList<LinkedList<Cell>>().apply {
+            add(headerRow)
+
+            didiDetailList.forEachIndexed { index, didiEntity ->
+                add(
+                    LinkedList<Cell>().apply {
+                        add(TextCell("${index + 1}", cellDataTextProperties, serialNumberCellWidth))
+                        add(
+                            TextCell(didiEntity.address, cellDataTextProperties, dataCellWidth)
+                        )
+                        add(
+                            TextCell(didiEntity.name, cellDataTextProperties, dataCellWidth)
+                        )
+                        add(
+                            TextCell(didiEntity.guardianName, cellDataTextProperties, dataCellWidth)
+                        )
+                        add(
+                            TextCell(didiEntity.cohortName, cellDataTextProperties, dataCellWidth)
+                        )
+                        add(
+                            TextCell(didiEntity.castName, cellDataTextProperties, dataCellWidth))
+                        add(
+                            TextCell(SHGFlag.fromInt(didiEntity.shgFlag).toString(), cellDataTextProperties, dataCellWidth))
+                    }
+                )
+            }
+        }
+
+        simplyPdfDocument.table.draw(rows, TableProperties().apply {
+            borderColor = "#000000"
+            borderWidth = 1
+            drawBorder = true
+        })
+
+        simplyPdfDocument.insertEmptyLines(1)
+
+        simplyPdfDocument.finish()
+
+    }
+
+    suspend fun getFormCPdg(context: Context, villageEntity: VillageEntity, didiDetailList: List<DidiEntity>, completionDate: String) {
+
+        val simplyPdfDocument = getSimplePdfDocument(context, villageEntity, "digital_form_c")
+
+        simplyPdfDocument.text.write("Digital Form C", titleTextProperties)
+
+        simplyPdfDocument.text.write("Final list of Ultra Poor families endorsed by VO (Village Organization)", subTitleTextProperties)
+
+        simplyPdfDocument.text.write("Village Name: ${villageEntity.name} \t VO Name: ${villageEntity.federationName}", contentTextProperties)
+
+        simplyPdfDocument.text.write("Date of CRP Drive: $completionDate", contentTextProperties)
+
+        simplyPdfDocument.text.write("Total no. Ultra-Poor families endorsed by VO: ${didiDetailList.size}", contentTextProperties)
+
+        simplyPdfDocument.insertEmptyLines(1)
+
+        val headerRow = LinkedList<Cell>().apply {
+            add(TextCell(SR_NO_HEADING_TEXT, cellDataTextProperties, serialNumberCellWidth))
+            add(TextCell(HOUSE_NO_HEADING_TEXT, cellDataTextProperties, dataCellWidth))
+            add(
+                TextCell(DIDI_NAME_HEADING_TEXT, cellDataTextProperties, dataCellWidth)
+            )
+            add(
+                TextCell(GUARDIAN_NAME_HEADING_TEXT, cellDataTextProperties, dataCellWidth)
+            )
+            add(
+                TextCell(TOLA_NAME_HEADING_TEXT, cellDataTextProperties, dataCellWidth)
+            )
+            add(
+                TextCell("Caste (ST/PVTG/SC/OBC/General)\n" +
+                        "(6)", cellDataTextProperties, dataCellWidth)
+            )
+            add(
+                TextCell("SHG member (Yes/ No)\n" +
+                        "(7)", cellDataTextProperties, dataCellWidth)
+            )
+        }
+
+        val rows = LinkedList<LinkedList<Cell>>().apply {
+            add(headerRow)
+
+            didiDetailList.forEachIndexed { index, didiEntity ->
+                add(
+                    LinkedList<Cell>().apply {
+                        add(TextCell("${index + 1}", cellDataTextProperties, serialNumberCellWidth))
+                        add(
+                            TextCell(didiEntity.address, cellDataTextProperties, dataCellWidth)
+                        )
+                        add(
+                            TextCell(didiEntity.name, cellDataTextProperties, dataCellWidth)
+                        )
+                        add(
+                            TextCell(didiEntity.guardianName, cellDataTextProperties, dataCellWidth)
+                        )
+                        add(
+                            TextCell(didiEntity.cohortName, cellDataTextProperties, dataCellWidth)
+                        )
+                        add(
+                            TextCell(didiEntity.castName, cellDataTextProperties, dataCellWidth))
+                        add(
+                            TextCell(SHGFlag.fromInt(didiEntity.shgFlag).toString(), cellDataTextProperties, dataCellWidth))
+                    }
+                )
+            }
+        }
+
+        simplyPdfDocument.table.draw(rows, TableProperties().apply {
+            borderColor = "#000000"
+            borderWidth = 1
+            drawBorder = true
+        })
+
+        simplyPdfDocument.insertEmptyLines(1)
+
+        simplyPdfDocument.finish()
+
+    }
+
+    private fun getSimplePdfDocument(context: Context, villageEntity: VillageEntity, fileName: String): SimplyPdfDocument {
+        return SimplyPdf.with(context, getPdfPath(context = context, formName = fileName, villageEntity.name))
+            .colorMode(DocumentInfo.ColorMode.COLOR)
+            .paperSize(PrintAttributes.MediaSize.ISO_A4)
+            .margin(Margin(margins, margins, margins, margins))
+            .paperOrientation(DocumentInfo.Orientation.LANDSCAPE)
+            .build()
+    }
+
+    private fun getPdfPath(context: Context, formName: String, villageName: String): File {
+        return File("${context.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)?.absolutePath}/${formName}_${villageName}.pdf")
+    }
+
+    private fun getTextPropertiesForCell(): TextProperties {
+        return TextProperties().apply {
+            textSize = 8
+            textColor = "#000000"
+            typeface = Typeface.DEFAULT
+            alignment = Layout.Alignment.ALIGN_CENTER
+        }
+    }
+
+    private fun getTitleTextProperties(): TextProperties {
+        return TextProperties().apply {
+            textSize = 13
+            textColor = "#000000"
+            typeface = Typeface.DEFAULT_BOLD
+            alignment = Layout.Alignment.ALIGN_CENTER
+
+        }
+    }
+
+    private fun getSubTitleTextProperties(): TextProperties {
+        return TextProperties().apply {
+            textSize = 10
+            textColor = "#000000"
+            typeface = Typeface.DEFAULT
+            alignment = Layout.Alignment.ALIGN_CENTER
+
+        }
+    }
+
+    private fun getContentTextProperties(): TextProperties {
+        return TextProperties().apply {
+            textSize = 8
+            textColor = "#000000"
+            typeface = Typeface.DEFAULT
+            alignment = Layout.Alignment.ALIGN_NORMAL
+
+        }
+    }
+}
