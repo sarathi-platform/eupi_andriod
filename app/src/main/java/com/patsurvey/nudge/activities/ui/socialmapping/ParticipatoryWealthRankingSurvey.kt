c4b92909f07650c742b27ef9d6721e314b6f69b1
anupambhardwaj <anupam.bhardwaj@siriusxm.com>
2023-07-28 18:51:25 +0530

    App Crash on camera open fix.

--- app/src/main/java/com/patsurvey/nudge/activities/ui/socialmapping/ParticipatoryWealthRankingSurvey.kt
+++ app/src/main/java/com/patsurvey/nudge/activities/ui/socialmapping/ParticipatoryWealthRankingSurvey.kt
@@ -243,7 +243,7 @@ fun ParticipatoryWealthRankingSurvey(
                 Box(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(vertical = 2.dp)
+                        .padding(vertical = 2.dp, horizontal = 4.dp)
                 ) {
                     Text(
                         text =
@@ -259,7 +259,7 @@ fun ParticipatoryWealthRankingSurvey(
                 Box(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(vertical = 2.dp)
+                        .padding(vertical = 2.dp, horizontal = 4.dp)
                 ) {
                     var count: Int = if (showDidiListForRank.first) {
                         when (showDidiListForRank.second) {
@@ -295,7 +295,7 @@ fun ParticipatoryWealthRankingSurvey(
                     )
                 }
 
-                AnimatedVisibility(visible = showDidiListForRank.first) {
+                AnimatedVisibility(visible = showDidiListForRank.first, modifier = Modifier.padding(horizontal = 4.dp)) {
 
                     Box(
                         modifier = Modifier
@@ -324,7 +324,8 @@ fun ParticipatoryWealthRankingSurvey(
                 }
 
                 Column(
-                    verticalArrangement = Arrangement.spacedBy(14.dp)
+                    verticalArrangement = Arrangement.spacedBy(14.dp),
+                    modifier = Modifier.padding(horizontal = 4.dp)
                 ) {
                     WealthRankingBox(
                         count = didids.value.filter { it.wealth_ranking == WealthRank.POOR.rank }.size,
