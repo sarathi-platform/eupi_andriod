edb72b2c3db52d8bd4e3de3c7a3ab97c330b407e
anupambhardwaj <anupam.bhardwaj@siriusxm.com>
2023-08-03 18:12:44 +0530

    Added fix for #297 & #251

--- app/src/main/java/com/patsurvey/nudge/activities/ui/selectlanguage/LanguageViewModel.kt
+++ app/src/main/java/com/patsurvey/nudge/activities/ui/selectlanguage/LanguageViewModel.kt
@@ -9,6 +9,7 @@ import com.patsurvey.nudge.database.dao.LanguageListDao
 import com.patsurvey.nudge.database.dao.VillageListDao
 import com.patsurvey.nudge.model.dataModel.ErrorModel
 import com.patsurvey.nudge.model.dataModel.ErrorModelWithApi
+import com.patsurvey.nudge.utils.NudgeLogger
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -36,22 +37,36 @@ class LanguageViewModel @Inject constructor(
 
     private fun fetchLanguageList() {
         job = CoroutineScope(Dispatchers.IO + exceptionHandler).launch {
-            val list=languageListDao.getAllLanguages()
-            withContext(Dispatchers.IO){
-                if (list.isNullOrEmpty()) {
-                    _languageList.value = listOf(
-                        LanguageEntity(
-                            id = 2,
-                            language = "English",
-                            langCode = "en",
-                            orderNumber = 1,
-                            localName = "English"
+            try {
+                val list=languageListDao.getAllLanguages()
+                withContext(Dispatchers.IO){
+                    if (list.isNullOrEmpty()) {
+                        _languageList.value = listOf(
+                            LanguageEntity(
+                                id = 2,
+                                language = "English",
+                                langCode = "en",
+                                orderNumber = 1,
+                                localName = "English"
+                            )
                         )
-                    )
-                } else {
-                    _languageList.value = list
+                    } else {
+                        _languageList.value = list
+                    }
                 }
+            } catch (ex: Exception) {
+                NudgeLogger.e("LanguageViewModel", "fetchLanguageList: ", ex)
+                _languageList.value = listOf(
+                    LanguageEntity(
+                        id = 2,
+                        language = "English",
+                        langCode = "en",
+                        orderNumber = 1,
+                        localName = "English"
+                    )
+                )
             }
+
         }
     }
     override fun onServerError(error: ErrorModel?) {
