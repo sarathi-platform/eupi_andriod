976ba93bbacbe975d6442638e28c02a4c6d78732
ankit <ankit.jain3@tothenew.com>
2024-02-22 12:42:37 +0530

    SARATHI-398 fix language bug in from screen

--- baselinesurvey/src/main/java/com/nrlm/baselinesurvey/database/dao/OptionItemDao.kt
+++ baselinesurvey/src/main/java/com/nrlm/baselinesurvey/database/dao/OptionItemDao.kt
@@ -6,7 +6,6 @@ import androidx.room.Query
 import com.nrlm.baselinesurvey.ANSWER_TABLE
 import com.nrlm.baselinesurvey.OPTION_TABLE
 import com.nrlm.baselinesurvey.database.entity.OptionItemEntity
-import com.nrlm.baselinesurvey.model.datamodel.OptionsItem
 
 @Dao
 interface OptionItemDao {
@@ -29,11 +28,12 @@ interface OptionItemDao {
         languageId: Int
     ): List<OptionItemEntity>
 
-    @Query("Select * from $OPTION_TABLE where  sectionId = :sectionId and surveyId = :surveyId and questionId = :questionId")
+    @Query("Select * from $OPTION_TABLE where  sectionId = :sectionId and surveyId = :surveyId and questionId = :questionId and languageId=:languageId")
     fun getSurveySectionQuestionOptions(
         sectionId: Int,
         surveyId: Int,
         questionId: Int,
+        languageId: Int
     ): List<OptionItemEntity>
 
     @Query("Update $OPTION_TABLE set isSelected = :isSelected where surveyId=:surveyId AND sectionId=:sectionId AND questionId = :questionId AND optionId = :optionId")
