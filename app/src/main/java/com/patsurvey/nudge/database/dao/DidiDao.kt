8d91e29b75aac54508481e3016f1167dc60f39d4
ankit <ankit.jain3@tothenew.com>
2024-03-21 16:06:03 +0530

    fixs bug

--- app/src/main/java/com/patsurvey/nudge/database/dao/DidiDao.kt
+++ app/src/main/java/com/patsurvey/nudge/database/dao/DidiDao.kt
@@ -23,7 +23,7 @@ interface DidiDao {
     @Query("SELECT * FROM $DIDI_TABLE where villageId = :villageId and activeStatus = 1 ORDER BY  cohortId ASC")
     fun getAllDidisForVillageWithoutDesc(villageId: Int): List<DidiEntity>
 
-    @Query("SELECT * FROM $DIDI_TABLE where villageId = :villageId and activeStatus = 1")
+    @Query("SELECT * FROM $DIDI_TABLE where villageId = :villageId and activeStatus = 1 order by localCreatedDate asc")
     fun getAllDidisForVillageWithoutOnlyDesc(villageId: Int): List<DidiEntity>
 
     @Query("Select * FROM $DIDI_TABLE where id = :id and activeStatus = 1")
