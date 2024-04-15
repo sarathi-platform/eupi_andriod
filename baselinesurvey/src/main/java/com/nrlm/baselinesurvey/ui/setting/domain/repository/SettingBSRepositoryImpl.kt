127d53a42d928ea9161f1104894fcb6515a8d102
ritik <ritik.singh@tothenew.com>
2024-04-15 17:20:13 +0530

    SARATHI-1718 working on baseline event regenration

--- baselinesurvey/src/main/java/com/nrlm/baselinesurvey/ui/setting/domain/repository/SettingBSRepositoryImpl.kt
+++ baselinesurvey/src/main/java/com/nrlm/baselinesurvey/ui/setting/domain/repository/SettingBSRepositoryImpl.kt
@@ -2,16 +2,23 @@ package com.nrlm.baselinesurvey.ui.setting.domain.repository
 
 import com.nrlm.baselinesurvey.BLANK_STRING
 import com.nrlm.baselinesurvey.LANGUAGE_OPEN_FROM_SETTING
+import com.nrlm.baselinesurvey.data.domain.EventWriterHelper
 import com.nrlm.baselinesurvey.data.prefs.PrefRepo
+import com.nrlm.baselinesurvey.database.NudgeBaselineDatabase
 import com.nrlm.baselinesurvey.model.response.ApiResponseModel
 import com.nrlm.baselinesurvey.network.interfaces.ApiService
 import com.nrlm.baselinesurvey.utils.BaselineCore
+import com.nrlm.baselinesurvey.utils.states.SectionStatus
+import com.nrlm.baselinesurvey.utils.states.SectionStatus.Companion.getSectionStatusNameFromOrdinal
+import com.nudge.core.database.entities.Events
 import com.nudge.core.getDefaultBackUpFileName
 import com.nudge.core.getDefaultImageBackUpFileName
 import com.nudge.core.preference.CoreSharedPrefs
 
 class SettingBSRepositoryImpl(private val prefRepo: PrefRepo,
                               private val apiService: ApiService,
+                              private val eventWriterHelper: EventWriterHelper,
+                              private val baselineDatabase: NudgeBaselineDatabase
     ):SettingBSRepository {
 
     override suspend fun performLogout(): ApiResponseModel<String> {
@@ -39,4 +46,22 @@ class SettingBSRepositoryImpl(private val prefRepo: PrefRepo,
     override fun saveLanguageScreenOpenFrom() {
         prefRepo.savePref(LANGUAGE_OPEN_FROM_SETTING,true)
     }
+
+    override suspend fun regenerateAllBaselineEvent() {
+        val events = ArrayList<Events>()
+    }
+
+    suspend fun regenerateMATStatusEvent(events: ArrayList<Events>) {
+        baselineDatabase.missionEntityDao().getMissions().forEach { missionEntity ->
+            events.add(
+                eventWriterHelper.createMissionStatusUpdateEvent(
+                    missionId = missionEntity.missionId,
+                    SectionStatus.valueOf(getSectionStatusNameFromOrdinal(missionEntity.missionStatus))
+                )
+            )
+        }
+        // baselineDatabase.missionActivityEntityDao().getActivities()
+    }
+
+
 }
