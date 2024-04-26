943edff68f8198360e59fe181914e40199ac4672
AnasToTheNew <114737789+AnasToTheNew@users.noreply.github.com>
2024-04-26 11:30:48 +0530

    Draft: SARATHI-1719,1633 Export/Import Functionality (#78)
    * Add new item as Load Server Data into Setting Screen to refresh the data.
    
    * add internet connection validation
    
    * SARATHI-1635 Prevent data loss on swipe to refresh in BPC
    Added form a, b and c and log in export file
    
    * SARATHI-1635 Added bpc error rate in summary screen
    Remove check for didi to mark complete or not available for submit and review
    Show didi for verification when review and submit already done for that village
    
    * Fixed Load server data issue in bpc
    
    * Fixed BPC Error Rate issue
    
    * fixed step status issue for CRP users
    
    * Changed message in bpc verification app
    
    * Fixed
    
    * Worked on training feedback for BPC verification
    BPC Training Feedback - Additional Status required "BPC Verification in Progress"
    
    * Fixes of when we refresh the village data, the status of BPC Verification in Progress changes to VO Endorsement Completed
    
    * Fixed Step List not updating correctly in swipe to refresh case
    
    * SARATHI-1719-Backup Local Data then reload fresh data from server.
    
    * SARATHI-1719 Make backup of local data then reload fresh data from server.
    
    * Implementation of Export Data functionality with share option.
    
    * Implementation of Export Data functionality with share option.
    
    * Fixed issue found during testing.
    
    * File Utils and Import Database
    
    * DropDown Option issue Fix
    
    * DropDown Option issue Fix (#66)
    
    * phone_persist_issue
    
    * fixed PDS and save button issue
    
    * Fixed issue found during testing.
    
    * handle userid when get null.
    
    * Fixed issue found during testing and version bump baseline v1.15(1).
    
    * Fixed the blocker issue SARATHI-1869 where activity and mission completion trigger were not enabling for not available didi's and version bump baseline v1.16(1).
    
    * Backup/Recovery option and Screen for export files,images,db and import db functionality.
    
    * Added changes for dropdown type question values field.
    
    * Added changes for dropdown type question values field.
    
    * Added changes for dropdown type question values field.
    
    * Fixed Import Functionality
    
    * Implement move zip files in public directory and import database fixes.
    
    * Comment Resolved
    
    ---------
    
    Co-authored-by: ritik <ritik.singh@tothenew.com>
    Co-authored-by: anupambhardwaj <anupam.bhardwaj@siriusxm.com>
    Co-authored-by: ankit <ankit.jain3@tothenew.com>
    Co-authored-by: Anupam Bhardwaj <60734262+Anupam-bhardwaj@users.noreply.github.com>

---
+++ baselinesurvey/src/main/java/com/nrlm/baselinesurvey/ui/backup/domain/repository/ExportImportRepository.kt
@@ -0,0 +1,11 @@
+package com.nrlm.baselinesurvey.ui.backup.domain.repository
+
+interface ExportImportRepository {
+  fun clearLocalData()
+  fun setAllDataSynced()
+
+    fun getUserMobileNumber():String
+    fun getUserID():String
+    fun getUserEmail():String
+
+}
--- baselinesurvey/src/main/java/com/nrlm/baselinesurvey/ui/setting/domain/repository/SettingBSRepository.kt
+++ baselinesurvey/src/main/java/com/nrlm/baselinesurvey/ui/setting/domain/repository/SettingBSRepository.kt
@@ -8,7 +8,15 @@ interface SettingBSRepository {
     fun clearSharedPref()
 
     fun saveLanguageScreenOpenFrom()
+
+    fun clearLocalData()
+
+    fun setAllDataSynced()
+
+    fun getUserMobileNumber():String
+    fun getUserID():String
+    fun getUserEmail():String
+
     fun getUserName(): String
-    fun getMobileNo(): String
 
 }
