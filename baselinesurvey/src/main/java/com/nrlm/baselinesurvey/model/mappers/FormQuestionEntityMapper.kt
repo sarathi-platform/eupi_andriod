9b961c9e3fba3506e10a4c07e2376f53891e0b54
ritiksingh-tothenew <114281671+ritiksingh-tothenew@users.noreply.github.com>
2024-05-04 13:38:54 +0530

    Removed Translation in Backup/Recovery screen (#100)
    * Removed Translation in Backup/Recovery screen
    Fixed multiselect dropdown values directly saving without converting into proper form
    
    * Resolved MR review points

--- baselinesurvey/src/main/java/com/nrlm/baselinesurvey/model/mappers/FormQuestionEntityMapper.kt
+++ baselinesurvey/src/main/java/com/nrlm/baselinesurvey/model/mappers/FormQuestionEntityMapper.kt
@@ -3,11 +3,15 @@ package com.nrlm.baselinesurvey.model.mappers
 import com.nrlm.baselinesurvey.BLANK_STRING
 import com.nrlm.baselinesurvey.DEFAULT_ID
 import com.nrlm.baselinesurvey.database.entity.FormQuestionResponseEntity
+import com.nrlm.baselinesurvey.database.entity.OptionItemEntity
 import com.nrlm.baselinesurvey.model.response.QuestionAnswerResponseModel
+import com.nrlm.baselinesurvey.utils.convertEventValueFromMultiSelectDropDownEvent
+import com.nrlm.baselinesurvey.utils.isMultiSelectDropdown
 
 object FormQuestionEntityMapper {
     fun getFormQuestionEntity(
         questionResponseModel: QuestionAnswerResponseModel,
+        optionsList: List<OptionItemEntity>,
     ): List<FormQuestionResponseEntity> {
         val formEntities = ArrayList<FormQuestionResponseEntity>()
         val optionItemEntities = OptionEntityMapper.getOptionEntitiesMapperForForm(
@@ -22,11 +26,19 @@ object FormQuestionEntityMapper {
                     optionId = optionItemEntity.optionId ?: DEFAULT_ID,
                     sectionId = questionResponseModel.sectionId.toInt(),
                     surveyId = questionResponseModel.surveyId,
-                    selectedValue = optionItemEntity.selectedValue ?: BLANK_STRING,
+                    selectedValue = if (isMultiSelectDropdown(
+                            optionsList,
+                            optionItemEntity.optionId
+                        )
+                    ) convertEventValueFromMultiSelectDropDownEvent(
+                        optionItemEntity.selectedValue ?: BLANK_STRING
+                    ) else optionItemEntity.selectedValue ?: BLANK_STRING,
                     questionId = questionResponseModel.question?.questionId ?: -1
                 )
+
             )
         }
         return formEntities
     }
+
 }
