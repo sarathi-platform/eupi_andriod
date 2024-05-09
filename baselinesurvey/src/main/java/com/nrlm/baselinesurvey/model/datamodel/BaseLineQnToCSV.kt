16654851b3a93338de5db17ab7c2f1de0bdb0f39
Atul Mishra <atul.mishra@tothenew.com>
2024-05-09 22:44:19 +0530

    commiting with seperating option with new row in csv.

--- baselinesurvey/src/main/java/com/nrlm/baselinesurvey/model/datamodel/BaseLineQnToCSV.kt
+++ baselinesurvey/src/main/java/com/nrlm/baselinesurvey/model/datamodel/BaseLineQnToCSV.kt
@@ -13,75 +13,99 @@ suspend fun List<SaveAnswerEventDto>.toCSVSave(
     didiDetailList: List<SurveyeeEntity>,
     optionItemDao: OptionItemDao
 ) : List<BaseLineQnATableCSV> = map {
-
+    val csvList = ArrayList<BaseLineQnATableCSV>()
     var response = ""
-        if (it.question.questionType.equals(
-                QuestionType.SingleSelectDropdown.name,
-                ignoreCase = true
-            )
-        ) {
+    this.forEach {
+        val didiName = didiDetailList.find { mIt -> mIt.didiId == it.subjectId }?.didiName
+        val dadaName = didiDetailList.find { mIt -> mIt.didiId == it.subjectId }?.dadaName
+        val houseNo = didiDetailList.find { mIt -> mIt.didiId == it.subjectId }?.houseNo
+        val villageName = didiDetailList.find { mIt -> mIt.didiId == it.subjectId }?.villageName
+        val cohoretName = didiDetailList.find { mIt -> mIt.didiId == it.subjectId }?.cohortName
 
-            val optionId = it.question.options.map { mIt -> mIt.optionId }
-            val selectedValueOption = optionItemDao.getOptions(
-                DEFAULT_LANGUAGE_ID,
-                optionId,
-                it.question.questionId,
-                it.sectionId,
-                it.surveyId
-            )
-            val selectedValue = selectedValueOption.flatMap { it.values!! }
-            val matchingValues = selectedValueOption.flatMap { optionItem ->
-                // For each option item, find values that match a certain condition, here it's just an example.
-                // You need to replace 'id' with the actual property from ValuesDto that should match optionItem.optionId.
-                selectedValue.filter { valuesDto ->
-                    valuesDto.id == optionItem.optionId
-                }
+        it.question.options.forEach {option ->
+                if (it.question.questionType.equals(
+                        QuestionType.SingleSelectDropdown.name,
+                        ignoreCase = true
+                    )
+                ) {
+
+                    val optionId = listOf(option.optionId)
+                    val selectedValueOption = optionItemDao.getOptions(
+                        DEFAULT_LANGUAGE_ID,
+                        optionId,
+                        it.question.questionId,
+                        it.sectionId,
+                        it.surveyId
+                    )
+                    val optionItemId = option.selectedValueWithIds
+                    val selectedValue = selectedValueOption.flatMap { it.values!! }
+                    var matchingValues: List<ValuesDto> = emptyList()
+                    if(!optionItemId.isNullOrEmpty()) {
+                        matchingValues = optionItemId.flatMap { optionItem ->
+                            // For each option item, find values that match a certain condition, here it's just an example.
+                            // You need to replace 'id' with the actual property from ValuesDto that should match optionItem.optionId.
+                            selectedValue.filter { valuesDto ->
+                                optionItem.id == valuesDto.id
+                            }
+                        }
+                    }else{
+                        matchingValues = selectedValueOption.flatMap { optionItem ->
+                            // For each option item, find values that match a certain condition, here it's just an example.
+                            // You need to replace 'id' with the actual property from ValuesDto that should match optionItem.optionId.
+                            selectedValue.filter { valuesDto ->
+                                valuesDto.id == optionItem.id
+                            }
+                        }
+                    }
+                    val selectedResponse = matchingValues.map { it.value }
+
+                    response = selectedResponse.joinToString("\n")
+                } else if (it.question.questionType.toLowerCase()
+                        .contains(QuestionType.Input.name.toLowerCase()) ||
+                    it.question.questionType.toLowerCase()
+                        .contains(QuestionType.YrsMonthPicker.name.toLowerCase())
+                ) {
+                    val selectedValues = option.selectedValue //it.question.options.map { mIt -> mIt.selectedValue }
+                    if (selectedValues != null) {
+                        response = selectedValues
+                    }
+                } else {
+                    val optionId = listOf(option.optionId)
+                    val selectedValue = optionItemDao.getOptions(
+                        DEFAULT_LANGUAGE_ID,
+                        optionId,
+                        it.question.questionId,
+                        it.sectionId,
+                        it.surveyId
+                    )
+                    val selectedResponse = selectedValue.map { it.display }
+                    response = selectedResponse.joinToString("\n")
+
+
             }
-            val selectedResponse = matchingValues.map { it.value }
-
-            response = selectedResponse.joinToString("\n")
-        } else if (it.question.questionType.toLowerCase()
-                .contains(QuestionType.Input.name.toLowerCase()) ||
-            it.question.questionType.toLowerCase()
-                .contains(QuestionType.YrsMonthPicker.name.toLowerCase())
-        ) {
-            val selectedValues = it.question.options.map { mIt -> mIt.selectedValue }
-            response = selectedValues.joinToString("\n")
-        } else {
-            val optionId = it.question.options.map { mIt -> mIt.optionId }
-            val selectedValue = optionItemDao.getOptions(
-                DEFAULT_LANGUAGE_ID,
-                optionId,
-                it.question.questionId,
-                it.sectionId,
-                it.surveyId
+            csvList.add(
+            BaseLineQnATableCSV(
+                id = it.question.questionId.toString(),
+                question = it.question.questionDesc,
+                subQuestion = option.optionDesc,
+                response = response,
+                subjectId = it.subjectId,
+                section = sectionList.find { sectionEntity -> sectionEntity.surveyId == it.surveyId && sectionEntity.sectionId == it.sectionId }?.sectionName,
+                didiName = didiName,
+                dadaName = dadaName,
+                house = houseNo,
+                cohoretName = cohoretName,
+                villageName = villageName,
+                surveyId = it.surveyId,
+                sectionId = it.sectionId,
             )
-            val selectedResponse = selectedValue.map { it.display }
-            response = selectedResponse.joinToString("\n")
-
+            )
+        }
     }
-    val didiName = didiDetailList.find { mIt -> mIt.didiId == it.subjectId }?.didiName
-    val dadaName = didiDetailList.find { mIt -> mIt.didiId == it.subjectId }?.dadaName
-    val houseNo = didiDetailList.find { mIt -> mIt.didiId == it.subjectId }?.houseNo
-    val villageName = didiDetailList.find { mIt -> mIt.didiId == it.subjectId }?.villageName
-    val cohoretName = didiDetailList.find { mIt -> mIt.didiId == it.subjectId }?.cohortName
-    val sub = it.question.options.map { mIt -> mIt.optionDesc }
-    val subject = sub.joinToString("\n")
-    BaseLineQnATableCSV(
-        id = it.question.questionId.toString(),
-        question = it.question.questionDesc,
-        subQuestion = subject,
-        response = response,
-        subjectId = it.subjectId,
-        section = sectionList.find { sectionEntity -> sectionEntity.surveyId == it.surveyId && sectionEntity.sectionId == it.sectionId }?.sectionName,
-        didiName = didiName,
-        dadaName = dadaName,
-        house = houseNo,
-        cohoretName = cohoretName,
-        villageName = villageName,
-        surveyId = it.surveyId,
-        sectionId = it.sectionId
-        )
+    return csvList
+
+
+
 }
 
 
