9f9adadf4176bff0007456efa4566ced13d0ac8b
ankit <ankit.jain3@tothenew.com>
2024-05-16 12:09:58 +0530

    added dataloading screen

--- missionActivityTask/src/main/java/com/sarathi/missionactivitytask/navigation/RootNavGraph.kt
+++ missionActivityTask/src/main/java/com/sarathi/missionactivitytask/navigation/RootNavGraph.kt
@@ -4,7 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.nrlm.baselinesurvey.navigation.home.HomeNavScreen
+import com.sarathi.missionactivitytask.navigation.home.HomeNavScreen
 
 
 @Composable
--- missionActivityTask/src/main/java/com/sarathi/missionactivitytask/navigation/home/HomeNavScreen.kt
+++ missionActivityTask/src/main/java/com/sarathi/missionactivitytask/navigation/home/HomeNavScreen.kt
@@ -1,4 +1,4 @@
-package com.nrlm.baselinesurvey.navigation.home
+package com.sarathi.missionactivitytask.navigation.home
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.padding
