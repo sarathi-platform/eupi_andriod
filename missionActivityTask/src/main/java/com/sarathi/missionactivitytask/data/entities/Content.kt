74719ca6f5e54970981f8a14263d938d6084a216
ankit <ankit.jain3@tothenew.com>
2024-05-21 16:44:13 +0530

    development in-progress

---
+++ missionActivityTask/src/main/java/com/sarathi/missionactivitytask/data/entities/Content.kt
@@ -0,0 +1,19 @@
+package com.sarathi.missionactivitytask.data.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.sarathi.missionactivitytask.constants.MissionActivityConstants.CONTENT_TABLE_NAME
+
+@Entity(tableName = CONTENT_TABLE_NAME)
+data class Content(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "contentId")
+    val contentId: Int,
+    val contentKey: String,
+    val contentValue: String,
+    val contentType: String,
+    var languageCode: Int,
+    var thumbUrl: String,
+    var isDownload: Int
+)
