70d00a85feaafa9ce45f169ec833d149f6198b25
Ankit-Kumar-Jain <156900393+Ankit-Kumar-Jain@users.noreply.github.com>
2024-06-07 15:30:44 +0530

    single and multiple dropdown component. (#150)
    * make single and multiple dropdown component.
    
    * single and multiple dropdown component.
    
    * single and multiple dropdown component.

---
+++ surveymanager/src/main/java/com/sarathi/surveymanager/ui/component/DropDownComponent.kt
@@ -0,0 +1,177 @@
+package com.sarathi.surveymanager.ui.component
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.PressInteraction
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.LayoutCoordinates
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.nudge.core.BLANK_STRING
+import com.nudge.core.ui.events.theme.blueDark
+import com.nudge.core.ui.events.theme.borderGrey
+import com.nudge.core.ui.events.theme.newMediumTextStyle
+import com.nudge.core.ui.events.theme.placeholderGrey
+import com.nudge.core.ui.events.theme.smallTextStyle
+import com.nudge.core.ui.events.theme.textColorDark
+import com.nudge.core.ui.events.theme.white
+import com.sarathi.dataloadingmangement.model.survey.response.ValuesDto
+import com.sarathi.surveymanager.R
+
+@Composable
+fun <T> DropDownComponent(
+    hint: String = stringResource(R.string.select),
+    items: List<T>,
+    modifier: Modifier,
+    dropDownBorder: Color = borderGrey,
+    dropDownBackground: Color = white,
+    selectedItem: String = BLANK_STRING,
+    expanded: Boolean = false,
+    mTextFieldSize: Size,
+    onExpandedChange: (Boolean) -> Unit,
+    onDismissRequest: () -> Unit,
+    onGlobalPositioned: (LayoutCoordinates) -> Unit,
+    onItemSelected: (T) -> Unit,
+) {
+    // Up Icon when expanded and down icon when collapsed
+    val icon = if (expanded)
+        Icons.Filled.KeyboardArrowUp
+    else
+        Icons.Filled.KeyboardArrowDown
+
+
+    val showLoader = remember {
+        mutableStateOf(false)
+    }
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.Start
+    ) {
+        CustomOutlineTextField(
+            value = selectedItem,
+            onValueChange = {
+            },
+            interactionSource = remember { MutableInteractionSource() }
+                .also { interactionSource ->
+                    LaunchedEffect(interactionSource) {
+                        interactionSource.interactions.collect {
+                            if (it is PressInteraction.Release) {
+                                // works like onClick
+                                onExpandedChange(expanded)
+                            }
+                        }
+                    }
+                },
+            readOnly = true,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 6.dp)
+                .clickable { onExpandedChange(expanded) }
+                .onGloballyPositioned { coordinates ->
+                    // This value is used to assign to
+                    // the DropDown the same width
+                    onGlobalPositioned(coordinates)
+//                    mTextFieldSize = coordinates.size.toSize()
+                },
+            textStyle = newMediumTextStyle,
+            singleLine = true,
+            maxLines = 1,
+            placeholder = {
+                Text(text = hint, style = newMediumTextStyle, color = placeholderGrey)
+            },
+            colors = TextFieldDefaults.textFieldColors(
+                textColor = textColorDark,
+                backgroundColor = Color.White,
+                focusedIndicatorColor = borderGrey,
+                unfocusedIndicatorColor = borderGrey,
+            ),
+            trailingIcon = {
+                Icon(icon, "contentDescription",
+                    Modifier.clickable { onExpandedChange(expanded) })
+            }
+        )
+
+        // Create a drop-down menu with list of cities,
+        // when clicked, set the Text Field text as the city selected
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { onDismissRequest() },
+            modifier = Modifier.width(with(LocalDensity.current) { mTextFieldSize.width.toDp() })
+        ) {
+
+            items.mapIndexed { index, item ->
+                val title: String
+                when (item) {
+                    is ValuesDto -> {
+                        title = item.value
+                    }
+
+                    else -> {
+                        title = item.toString()
+                    }
+                }
+                DropdownMenuItem(onClick = {
+                    onItemSelected(item)
+                }) {
+
+                    Text(
+                        text = title,
+                        color = blueDark,
+                        modifier = Modifier.fillMaxWidth(),
+                        style = smallTextStyle
+                    )
+                }
+            }
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DropDownWithTittleCompoentPerview() {
+    var casteTextFieldSize by remember { mutableStateOf(Size.Zero) }
+    val screens = listOf(
+        "Setting",
+        "Question",
+        "SingleQuestion",
+        "DigitalFormA",
+        "DigitalFormB",
+        "DigitalFormC",
+        "Login",
+        "Other"
+    )
+    DropDownComponent(
+        items = screens,
+        modifier = Modifier.padding(10.dp),
+        mTextFieldSize = casteTextFieldSize,
+        onExpandedChange = { },
+        onDismissRequest = { },
+        onGlobalPositioned = {},
+        onItemSelected = {},
+    )
+}
---
+++ surveymanager/src/main/java/com/sarathi/surveymanager/ui/component/TypeDropDownComponent.kt
@@ -0,0 +1,72 @@
+package com.sarathi.surveymanager.ui.component
+
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.toSize
+import com.nudge.core.showCustomToast
+import com.sarathi.dataloadingmangement.model.survey.response.ValuesDto
+import com.sarathi.surveymanager.R
+
+@Composable
+fun TypeDropDownComponent(
+    hintText: String = stringResource(R.string.select),
+    sources: List<ValuesDto>?,
+    isEditAllowed: Boolean = true,
+    selectOptionText: Int = 0,
+    onAnswerSelection: (selectValue: Int) -> Unit
+) {
+    val context = LocalContext.current
+    val defaultSourceList =
+        sources ?: listOf(ValuesDto(id = 1, "Yes"), ValuesDto(id = 2, "No"))
+    var expanded by remember { mutableStateOf(false) }
+    var selectedOptionText by remember {
+        mutableStateOf(
+            if (selectOptionText == 0) hintText else defaultSourceList.find { it.id == selectOptionText }?.value
+                ?: hintText
+        )
+    }
+//    if (!defaultSourceList.contains(selectedOptionText.)) {
+//        selectedOptionText = BLANK_STRING
+//    }
+
+    var textFieldSize by remember { mutableStateOf(Size.Zero) }
+
+
+    DropDownComponent(items = defaultSourceList,
+        modifier = Modifier.fillMaxWidth(),
+        mTextFieldSize = textFieldSize,
+        expanded = expanded,
+        selectedItem = selectedOptionText,
+        onExpandedChange = {
+            expanded = !it
+        },
+        onDismissRequest = {
+            expanded = false
+        },
+        onGlobalPositioned = { coordinates ->
+            textFieldSize = coordinates.size.toSize()
+        },
+        onItemSelected = {
+            if (isEditAllowed) {
+                selectedOptionText =
+                    defaultSourceList[defaultSourceList.indexOf(it)].value
+                onAnswerSelection(it.id)
+                expanded = false
+            } else {
+                showCustomToast(
+                    context,
+                    context.getString(R.string.edit_disable_message)
+                )
+            }
+        })
+
+}
