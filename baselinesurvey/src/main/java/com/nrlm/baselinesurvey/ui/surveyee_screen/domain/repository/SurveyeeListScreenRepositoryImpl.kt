d60d404215af12912d4f165133017e1ecc2bf63c
anas <anas.mansoori@tothenew.com>
2024-06-20 16:30:07 +0530

    SARATHI-2217 Folder and Event File name fixed.

--- baselinesurvey/src/main/java/com/nrlm/baselinesurvey/ui/surveyee_screen/domain/repository/SurveyeeListScreenRepositoryImpl.kt
+++ baselinesurvey/src/main/java/com/nrlm/baselinesurvey/ui/surveyee_screen/domain/repository/SurveyeeListScreenRepositoryImpl.kt
@@ -9,6 +9,7 @@ import com.nrlm.baselinesurvey.PREF_KEY_PROFILE_IMAGE
 import com.nrlm.baselinesurvey.PREF_KEY_ROLE_NAME
 import com.nrlm.baselinesurvey.PREF_KEY_TYPE_NAME
 import com.nrlm.baselinesurvey.PREF_KEY_USER_NAME
+import com.nrlm.baselinesurvey.PREF_MOBILE_NUMBER
 import com.nrlm.baselinesurvey.SUCCESS
 import com.nrlm.baselinesurvey.data.prefs.PrefRepo
 import com.nrlm.baselinesurvey.database.dao.ActivityTaskDao
@@ -19,11 +20,13 @@ import com.nrlm.baselinesurvey.database.entity.ActivityTaskEntity
 import com.nrlm.baselinesurvey.database.entity.MissionActivityEntity
 import com.nrlm.baselinesurvey.database.entity.SurveyeeEntity
 import com.nrlm.baselinesurvey.network.interfaces.ApiService
+import com.nrlm.baselinesurvey.utils.BaselineCore
 import com.nrlm.baselinesurvey.utils.createMultiLanguageVillageRequest
 import com.nrlm.baselinesurvey.utils.states.SectionStatus
 import com.nrlm.baselinesurvey.utils.states.SurveyState
 import com.nrlm.baselinesurvey.utils.states.SurveyeeCardState
 import com.nudge.core.toDate
+import com.nudge.core.updateCoreEventFileName
 import javax.inject.Inject
 
 class SurveyeeListScreenRepositoryImpl @Inject constructor(
@@ -73,13 +76,18 @@ class SurveyeeListScreenRepositoryImpl @Inject constructor(
             val userApiResponse = apiService.userAndVillageListAPI(languageId = userViewApiRequest)
             if (userApiResponse.status.equals(SUCCESS, true)) {
                 userApiResponse.data?.let {
-                    prefRepo.savePref(PREF_KEY_USER_NAME, it.username ?: "")
-                    prefRepo.savePref(PREF_KEY_NAME, it.name ?: "")
-                    prefRepo.savePref(PREF_KEY_EMAIL, it.email ?: "")
-                    prefRepo.savePref(PREF_KEY_IDENTITY_NUMBER, it.identityNumber ?: "")
-                    prefRepo.savePref(PREF_KEY_PROFILE_IMAGE, it.profileImage ?: "")
-                    prefRepo.savePref(PREF_KEY_ROLE_NAME, it.roleName ?: "")
-                    prefRepo.savePref(PREF_KEY_TYPE_NAME, it.typeName ?: "")
+                    prefRepo.savePref(PREF_KEY_USER_NAME, it.username ?: BLANK_STRING)
+                    prefRepo.savePref(PREF_KEY_NAME, it.name ?: BLANK_STRING)
+                    prefRepo.savePref(PREF_KEY_EMAIL, it.email ?: BLANK_STRING)
+                    prefRepo.savePref(PREF_KEY_IDENTITY_NUMBER, it.identityNumber ?: BLANK_STRING)
+                    prefRepo.savePref(PREF_KEY_PROFILE_IMAGE, it.profileImage ?: BLANK_STRING)
+                    prefRepo.savePref(PREF_KEY_ROLE_NAME, it.roleName ?: BLANK_STRING)
+                    prefRepo.savePref(PREF_KEY_TYPE_NAME, it.typeName ?: BLANK_STRING)
+
+                    updateCoreEventFileName(
+                        context = BaselineCore.getAppContext(),
+                        mobileNo = prefRepo.getPref(PREF_MOBILE_NUMBER, BLANK_STRING) ?: BLANK_STRING
+                    )
                 }
                 val apiResponse = userApiResponse.data?.username?.toInt()
                     ?.let { apiService.getDidisFromNetwork(userId = it) }
