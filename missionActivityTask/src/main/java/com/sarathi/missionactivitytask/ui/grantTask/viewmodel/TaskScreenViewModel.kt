97f54bec0a605f05fe4fb3a5285992baae7594cb
ritik <ritik.singh@tothenew.com>
2024-07-15 16:23:05 +0530

    SARATHI-2489 Fixed search is not working for receipt of fund

--- missionActivityTask/src/main/java/com/sarathi/missionactivitytask/ui/grantTask/viewmodel/TaskScreenViewModel.kt
+++ missionActivityTask/src/main/java/com/sarathi/missionactivitytask/ui/grantTask/viewmodel/TaskScreenViewModel.kt
@@ -63,7 +63,7 @@ open class TaskScreenViewModel @Inject constructor(
     val filterList: State<HashMap<Int, HashMap<String, TaskCardModel>>> get() = _filterList
     val searchLabel = mutableStateOf<String>(BLANK_STRING)
     val isButtonEnable = mutableStateOf<Boolean>(false)
-    var isGroupByEnable = mutableStateOf(true)
+    var isGroupByEnable = mutableStateOf(false)
     var isFilterEnable = mutableStateOf(false)
     var isActivityCompleted = mutableStateOf(false)
 
@@ -140,6 +140,7 @@ open class TaskScreenViewModel @Inject constructor(
                     if ((uiComponent[GrantTaskCardSlots.GRANT_GROUP_BY.name]?.value
                             ?: BLANK_STRING).isNotBlank()
                     ) {
+                        isGroupByEnable.value = true
                         isFilterEnable.value = true
                     }
                 }
