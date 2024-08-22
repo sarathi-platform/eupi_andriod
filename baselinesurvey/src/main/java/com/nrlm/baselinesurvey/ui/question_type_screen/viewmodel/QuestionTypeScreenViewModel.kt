197071ccdaa9a5e28bb2963bb63b5b5b196a7259
anupambhardwaj <anupam.bhardwaj@siriusxm.com>
2024-08-22 13:59:31 +0530

    SARATHI-2495: Removed unnecessary logs.

--- baselinesurvey/src/main/java/com/nrlm/baselinesurvey/ui/question_type_screen/viewmodel/QuestionTypeScreenViewModel.kt
+++ baselinesurvey/src/main/java/com/nrlm/baselinesurvey/ui/question_type_screen/viewmodel/QuestionTypeScreenViewModel.kt
@@ -41,7 +41,6 @@ import com.nrlm.baselinesurvey.utils.convertToOptionItemEntity
 import com.nrlm.baselinesurvey.utils.findIndexOfListByOptionId
 import com.nrlm.baselinesurvey.utils.findOptionExist
 import com.nrlm.baselinesurvey.utils.isNumeric
-import com.nrlm.baselinesurvey.utils.json
 import com.nrlm.baselinesurvey.utils.showCustomToast
 import com.nrlm.baselinesurvey.utils.states.DialogState
 import com.nrlm.baselinesurvey.utils.states.LoaderState
@@ -276,10 +275,7 @@ class QuestionTypeScreenViewModel @Inject constructor(
                 }
             }
         }
-        Log.d(
-            TAG,
-            "getOptionsInDefaultLanguage: updatedOptionListInDefaultLanguage -> ${updatedOptionListInDefaultLanguage.json()}"
-        )
+
     }
 
     private fun getOptionItemEntityState(
