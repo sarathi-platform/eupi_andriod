4dfb7aa918024820ad5fd9693154593bd6ba0901
ritik <ritik.singh@tothenew.com>
2024-08-29 15:36:09 +0530

    SARATHI-2880 updating the village list for unassignment and village name and federationNAme

--- app/src/main/java/com/patsurvey/nudge/database/dao/VillageListDao.kt
+++ app/src/main/java/com/patsurvey/nudge/database/dao/VillageListDao.kt
@@ -11,16 +11,16 @@ import com.patsurvey.nudge.utils.VILLAGE_TABLE_NAME
 @Dao
 interface VillageListDao {
 
-    @Query("SELECT * FROM $VILLAGE_TABLE_NAME where languageId=:languageId")
+    @Query("SELECT * FROM $VILLAGE_TABLE_NAME where languageId=:languageId and isActive=1")
     fun getAllVillages(languageId:Int): List<VillageEntity>
 
-    @Query("SELECT * FROM $VILLAGE_TABLE_NAME")
+    @Query("SELECT * FROM $VILLAGE_TABLE_NAME where isActive=1")
     fun getAllLanguageVillages(): List<VillageEntity>
 
-    @Query("Select * FROM $VILLAGE_TABLE_NAME where id = :id")
+    @Query("Select * FROM $VILLAGE_TABLE_NAME where id = :id and isActive=1")
     fun getVillage(id: Int): VillageEntity
 
-    @Query("Select * FROM $VILLAGE_TABLE_NAME where id = :id and languageId=:languageId")
+    @Query("Select * FROM $VILLAGE_TABLE_NAME where id = :id and languageId=:languageId and isActive=1")
     fun getVillageFromId(id: Int, languageId: Int): VillageEntity?
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertVillage(village: VillageEntity)
@@ -30,10 +30,18 @@ interface VillageListDao {
 
     @Transaction()
     fun insertOnlyNewData(villages: List<VillageEntity>, userBPC: Boolean) {
+        softDeleteVillage()
         villages.forEach {
             val localVillage = getVillageFromId(it.id, it.languageId)
             if (localVillage == null) {
                 insertVillage(it)
+            } else if (!userBPC) {
+                updateVillageData(
+                    name = it.name,
+                    federationName = it.federationName,
+                    villageId = it.id,
+                    languageId = it.languageId
+                )
             } else if (localVillage.isDataLoadTriedOnce != 1 && userBPC) {
                 deleteVillageById(localVillage.localVillageId)
                 insertVillage(it)
@@ -41,12 +49,18 @@ interface VillageListDao {
         }
     }
 
+    @Query("update $VILLAGE_TABLE_NAME set isActive=2")
+    fun softDeleteVillage()
+
     @Query("DELETE from $VILLAGE_TABLE_NAME where localVillageId= :localVillageId")
     fun deleteVillageById(localVillageId: Int)
     @Query("UPDATE $VILLAGE_TABLE_NAME SET steps_completed = :stepId where id = :villageId")
     fun updateLastCompleteStep(villageId: Int, stepId: List<Int>)
 
-    @Query("Select * from $VILLAGE_TABLE_NAME where id = :villageId AND languageId = :languageId")
+    @Query("UPDATE $VILLAGE_TABLE_NAME SET name=:name,federationName=:federationName, isActive=1 where  id = :villageId and languageId=:languageId  ")
+    fun updateVillageData(villageId: Int, languageId: Int, name: String, federationName: String)
+
+    @Query("Select * from $VILLAGE_TABLE_NAME where id = :villageId AND languageId = :languageId  and isActive=1")
     fun fetchVillageDetailsForLanguage(villageId: Int, languageId: Int):VillageEntity
     @Query("DELETE from $VILLAGE_TABLE_NAME")
     fun deleteAllVilleges()
