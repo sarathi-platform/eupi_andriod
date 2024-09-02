5051363e4e912c17c29661edecf3ea286f6a6f45
ankit <ankit.jain3@tothenew.com>
2024-09-02 14:56:58 +0530

    MigrationQueries for activity_config_table.

--- DataManager/src/main/java/com/sarathi/dataloadingmangement/data/database/MigrationQueries.kt
+++ DataManager/src/main/java/com/sarathi/dataloadingmangement/data/database/MigrationQueries.kt
@@ -1,6 +1,5 @@
 package com.sarathi.dataloadingmangement.data.database
 
-import com.sarathi.dataloadingmangement.ACTIVITY_CONFIG_TABLE_NAME
 import com.sarathi.dataloadingmangement.ASSETS_TABLE_NAME
 import com.sarathi.dataloadingmangement.ASSET_JOURNAL_TABLE_NAME
 import com.sarathi.dataloadingmangement.LIVELIHOOD_EVENT_TABLE_NAME
@@ -15,7 +14,7 @@ object MigrationQueries {
     val CREATE_MONEY_JOUNRAL_TABLE =
         "CREATE TABLE IF NOT EXISTS $MONEY_JOURNAL_TABLE_NAME (`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `userId` TEXT NOT NULL, `transactionId` TEXT NOT NULL, `transactionDate` INTEGER NOT NULL, `transactionDetails` TEXT NOT NULL, `transactionFlow` TEXT NOT NULL, `transactionType` TEXT NOT NULL, `transactionAmount` REAL NOT NULL, `referenceId` INTEGER NOT NULL, `referenceType` TEXT NOT NULL, `subjectId` INTEGER NOT NULL, `subjectType` TEXT NOT NULL, `status` INTEGER NOT NULL, `modifiedDate` INTEGER NOT NULL,`createdDate` INTEGER NOT NULL)"
     val CREATE_ASSET_JOURNAL_TABLE = "CREATE TABLE IF NOT EXISTS $ASSET_JOURNAL_TABLE_NAME (\n" +
-            "    'id' INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
+            "    'id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n" +
             "    userId TEXT NOT NULL,\n" +
             "    transactionId TEXT NOT NULL,\n" +
             "    transactionDate INTEGER NOT NULL,\n" +
@@ -34,7 +33,7 @@ object MigrationQueries {
             ")"
     val CREATE_LIVELIHOOD_EVENT_MAPPING_TABLE =
         "CREATE TABLE IF NOT EXISTS $SUBJECT_LIVELIHOOD_EVENT_MAPPING_TABLE_NAME (\n" +
-                "    'id' INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
+                "    'id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n" +
                 "    userId TEXT NOT NULL,\n" +
                 "    transactionId TEXT NOT NULL,\n" +
                 "    subjectId INTEGER NOT NULL,\n" +
@@ -49,7 +48,7 @@ object MigrationQueries {
                 ")"
 
     val CREATE_PRODUCT_CONFIG_TABLE = "CREATE TABLE IF NOT EXISTS $PRODUCT_TABLE_NAME (\n" +
-            "    'id' INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
+            "    'id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n" +
             "    productId INTEGER NOT NULL,\n" +
             "    livelihoodId INTEGER NOT NULL,\n" +
             "    userId TEXT NOT NULL,\n" +
@@ -59,7 +58,7 @@ object MigrationQueries {
             ")"
 
     val CREATE_LIVELIHOOD_ASSET_TABLE = "CREATE TABLE IF NOT EXISTS $ASSETS_TABLE_NAME (\n" +
-            "    'id' INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
+            "    'id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n" +
             "    assetId INTEGER NOT NULL,\n" +
             "    livelihoodId INTEGER NOT NULL,\n" +
             "    userId TEXT NOT NULL,\n" +
@@ -71,7 +70,7 @@ object MigrationQueries {
             ")"
     val CREATE_LIVELIHOOD_EVENT_TABLE =
         "CREATE TABLE IF NOT EXISTS  $LIVELIHOOD_EVENT_TABLE_NAME (\n" +
-                "    'id' INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
+                "    'id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n" +
                 "    eventId INTEGER NOT NULL,\n" +
                 "    livelihoodId INTEGER NOT NULL,\n" +
                 "    userId TEXT NOT NULL,\n" +
@@ -81,7 +80,7 @@ object MigrationQueries {
                 ")"
 
     val CREATE_LIVELIHOOD_TABLE = "CREATE TABLE IF NOT EXISTS  $LIVELIHOOD_TABLE_NAME (\n" +
-            "    'id' INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
+            "    'id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n" +
             "    livelihoodId INTEGER NOT NULL,\n" +
             "    userId TEXT NOT NULL,\n" +
             "    name TEXT NOT NULL,\n" +
@@ -92,7 +91,7 @@ object MigrationQueries {
 
     val CREATE_LIVELIHOOD_LANGUAGE_REFRENCE_TABLE =
         "CREATE TABLE  IF NOT EXISTS  $LIVELIHOOD_LANGUAGE_TABLE_NAME (\n" +
-                "    'id' INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
+                "    'id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n" +
                 "    livelihoodId INTEGER NOT NULL,\n" +
                 "    referenceType TEXT NOT NULL,\n" +
                 "    languageCode TEXT NOT NULL,\n" +
@@ -102,16 +101,15 @@ object MigrationQueries {
 
     val CREATE_SUBJECT_LIVELIHOOD_MAPPING_TABLE_ =
         "CREATE TABLE IF NOT EXISTS  $SUBJECT_LIVELIHOOD_MAPPING_TABLE_NAME (\n" +
-                "    'id' INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
+                "    'id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n" +
                 "    userId TEXT NOT NULL,\n" +
                 "    subjectId INTEGER NOT NULL,\n" +
                 "    livelihoodId INTEGER NOT NULL,\n" +
                 "    type INTEGER NOT NULL,\n" +
                 "    status INTEGER NOT NULL\n" +
+                " primaryLivelihoodId INTEGER NOT NULL\n " +     //Todo Remove After livelihood income Expense Refactor
+                " secondaryLivelihoodId INTEGER NOT NULL\n " +     //Todo Remove After livelihood income Expense Refactor
                 ")"
 
-
-    val ALTER_ACTIVITY_CONFIG_TABLE =
-        "ALTER TABLE $ACTIVITY_CONFIG_TABLE_NAME ADD COLUMN 'activityType' TEXT,ADD COLUMN 'activityTypeId'  INTEGER"
 }
 
