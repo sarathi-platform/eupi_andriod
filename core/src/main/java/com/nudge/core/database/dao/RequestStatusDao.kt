41632529e2581cd23b9ba35bd7af0405221cd9d2
anas <anas.mansoori@tothenew.com>
2024-09-06 14:56:45 +0530

    Add Sync Migration rules and Update Column name use camelCase

--- core/src/main/java/com/nudge/core/database/dao/RequestStatusDao.kt
+++ core/src/main/java/com/nudge/core/database/dao/RequestStatusDao.kt
@@ -6,7 +6,7 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Transaction
 import com.nudge.core.BLANK_STRING
-import com.nudge.core.RequestStatusTable
+import com.nudge.core.REQUEST_STATUS_TABLE_NAME
 import com.nudge.core.database.entities.RequestStatusEntity
 import java.util.Date
 
@@ -18,10 +18,10 @@ interface RequestStatusDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(requestStatusList: List<RequestStatusEntity>)
 
-    @Query("SELECT * from $RequestStatusTable")
+    @Query("SELECT * from $REQUEST_STATUS_TABLE_NAME")
     fun getAllRequestEvent(): List<RequestStatusEntity>
 
-    @Query("UPDATE $RequestStatusTable SET eventCount =:eventCount,status=:status,modifiedDate =:modifiedDate WHERE requestId =:requestId AND mobileNumber =:mobileNumber")
+    @Query("UPDATE $REQUEST_STATUS_TABLE_NAME SET eventCount =:eventCount,status=:status,modifiedDate =:modifiedDate WHERE requestId =:requestId AND mobileNumber =:mobileNumber")
     fun updateEventRequestIdStatus(
         eventCount: Int,
         requestId: String,
@@ -30,10 +30,10 @@ interface RequestStatusDao {
         mobileNumber: String
     )
 
-    @Query("SELECT COUNT(*) FROM $RequestStatusTable WHERE requestId =:requestId AND mobileNumber =:mobileNumber")
+    @Query("SELECT COUNT(*) FROM $REQUEST_STATUS_TABLE_NAME WHERE requestId =:requestId AND mobileNumber =:mobileNumber")
     fun fetchRequestIdCount(requestId: String, mobileNumber: String): Int
 
-    @Query("SELECT * from $RequestStatusTable WHERE mobileNumber =:mobileNumber AND status in (:status)")
+    @Query("SELECT * from $REQUEST_STATUS_TABLE_NAME WHERE mobileNumber =:mobileNumber AND status in (:status)")
     fun getAllRequestEventForConsumerStatus(
         mobileNumber: String,
         status: List<String>
