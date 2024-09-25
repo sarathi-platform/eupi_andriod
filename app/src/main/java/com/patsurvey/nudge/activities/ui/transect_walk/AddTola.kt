3357ccd98a0867e091e909e0e077c38d8c82c864
aayushi <aayushi.agarwal@tothenew.com>
2024-09-25 13:02:13 +0530

    click get Location button show dialogue if gps not available

--- app/src/main/java/com/patsurvey/nudge/activities/ui/transect_walk/AddTola.kt
+++ app/src/main/java/com/patsurvey/nudge/activities/ui/transect_walk/AddTola.kt
@@ -722,7 +722,7 @@ fun TolaBox(
                                                 )
                                             }
                                         }
-//
+
                                    getLocation(
                                         context = context,
                                         gpsConsumer,
