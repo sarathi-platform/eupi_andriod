a4299a7b6f80f5d55ec65e69417ccd238b93657b
ritik <ritik.singh@tothenew.com>
2024-09-26 16:10:48 +0530

    Fixed batch size calculation using payload

--- core/src/main/java/com/nudge/core/enums/SyncBatchEnum.kt
+++ core/src/main/java/com/nudge/core/enums/SyncBatchEnum.kt
@@ -1,10 +1,11 @@
 package com.nudge.core.enums
 
 enum class SyncBatchEnum(val batchSize: Int, val maxPayloadSize: Long) {
-    POOR(batchSize = 5, maxPayloadSize = 150),
-    MODERATE(batchSize = 10, maxPayloadSize = 550),
-    GOOD(batchSize = 15, maxPayloadSize = 2000),
-    EXCELLENT(batchSize = 20, maxPayloadSize = 5000),
-    UNKNOWN(batchSize = 3, maxPayloadSize = 100)
+    //speed in kbps
+    POOR(batchSize = 5, maxPayloadSize = 30),
+    MODERATE(batchSize = 10, maxPayloadSize = 60),
+    GOOD(batchSize = 15, maxPayloadSize = 90),
+    EXCELLENT(batchSize = 20, maxPayloadSize = 150),
+    UNKNOWN(batchSize = 3, maxPayloadSize = 20)
 
 }
