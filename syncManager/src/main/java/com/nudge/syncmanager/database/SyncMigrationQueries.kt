fa4aaeb17337050f435752910f4a302d4f736b6c
Ankit-Kumar-Jain <156900393+Ankit-Kumar-Jain@users.noreply.github.com>
2024-10-17 10:35:55 +0530

    Migration issue (#378)
    * migration on sync
    
    * migration issue has been fix on IMAGE_STATUS_TABLE_NAME, CREATE_REQUEST_STATUS_TABLE and CREATE_EVENT_STATUS_TABLE

--- syncManager/src/main/java/com/nudge/syncmanager/database/SyncMigrationQueries.kt
+++ syncManager/src/main/java/com/nudge/syncmanager/database/SyncMigrationQueries.kt
@@ -5,6 +5,11 @@ import com.nudge.core.IMAGE_STATUS_TABLE_NAME
 import com.nudge.core.REQUEST_STATUS_TABLE_NAME
 
 object SyncMigrationQueries {
+    val ALTER_EVENT_TABLE_COLUMN_RESULT_DROP =
+        "ALTER TABLE 'events_table' DROP COLUMN result"
+
+    val ALTER_EVENT_TABLE_COLUMN_CONSUMER_STATUS_DROP =
+        "ALTER TABLE 'events_table' DROP COLUMN consumer_status"
 
     const val ADD_REQUEST_ID_IN_EVENT_TABLE =
         "ALTER TABLE 'events_table' ADD COLUMN 'requestId' TEXT"
@@ -25,32 +30,32 @@ object SyncMigrationQueries {
 
     const val CREATE_IMAGE_STATUS_TABLE =
         "CREATE TABLE IF NOT EXISTS $IMAGE_STATUS_TABLE_NAME (\n" +
-                "    'id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n" +
+                "id TEXT NOT NULL PRIMARY KEY,\n" +
                 "    name TEXT NOT NULL,\n" +
                 "    type TEXT NOT NULL,\n" +
-                "    createdDate INTEGER NOT NULL,\n" +
+                "    createDate INTEGER,\n" +
                 "    modifiedDate INTEGER NOT NULL,\n" +
                 "    createdBy TEXT NOT NULL,\n" +
                 "    mobileNumber TEXT NOT NULL,\n" +
                 "    fileName TEXT,\n" +
                 "    filePath TEXT,\n" +
                 "    status TEXT NOT NULL,\n" +
-                "    retryCount INTEGER NOT NULL DEFAULT 0,\n" +
-                "    requestId TEXT,\n" +
+                "    retryCount INTEGER,\n" +
                 "    errorMessage TEXT,\n" +
-                "    imageEventId TEXT NOT NULL,\n" +
-                "    blobUrl TEXT \n" +
+                "    imageEventId TEXT,\n" +
+                "    requestId TEXT,\n" +
+                "    blobUrl TEXT\n" +
                 ")"
 
     const val CREATE_REQUEST_STATUS_TABLE =
         "CREATE TABLE IF NOT EXISTS $REQUEST_STATUS_TABLE_NAME (\n" +
-                "    'id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n" +
-                "    status TEXT NOT NULL,\n" +
+                "'id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n" +
+                "    status TEXT,\n" +
                 "    requestId TEXT,\n" +
-                "    createdDate INTEGER NOT NULL,\n" +
+                "    createdDate INTEGER,\n" +
                 "    modifiedDate INTEGER NOT NULL,\n" +
                 "    createdBy TEXT NOT NULL,\n" +
                 "    mobileNumber TEXT NOT NULL,\n" +
-                "    eventCount INTEGER NOT NULL DEFAULT 0\n" +
+                "    eventCount INTEGER\n" +
                 ")"
 }
