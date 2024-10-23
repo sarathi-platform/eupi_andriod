400441b1d5cf403033834b73a75b8c9ced50629c
anupambhardwaj <anupam.bhardwaj@siriusxm.com>
2024-10-23 16:54:41 +0530

    SARATHI-2678: Added code to create event for SHG Flag and Able Bodied flag and allow edits for the same.

--- app/src/main/java/com/patsurvey/nudge/model/dataModel/DidiFlagEventModel.kt
+++ app/src/main/java/com/patsurvey/nudge/model/dataModel/DidiFlagEventModel.kt
@@ -6,7 +6,7 @@ import com.patsurvey.nudge.database.DidiEntity
 import com.patsurvey.nudge.utils.AbleBodiedFlag
 import com.patsurvey.nudge.utils.SHGFlag
 
-data class DidiFlagEventModel(
+data class ShgFlagEventModel(
 
     @SerializedName("villageId") val villageId: Int,
     @SerializedName("name") var name: String = BLANK_STRING,
@@ -16,17 +16,16 @@ data class DidiFlagEventModel(
     @SerializedName("cohortName") var cohortName: String = BLANK_STRING,
     @SerializedName("deviceId") var deviceId: String,
     @SerializedName("cohortDeviceId") var cohortDeviceId: String? = BLANK_STRING,
-    @SerializedName("shgFlag") var shgFlag: String,
-    @SerializedName("ableBodiedFlag") var ableBodiedFlag: String
+    @SerializedName("shgFlag") var shgFlag: String
 ) {
 
     companion object {
 
-        fun getDidiFlagEventModel(
+        fun getShgFlagEventModel(
             didiEntity: DidiEntity,
             tolaDeviceId: String
-        ): DidiFlagEventModel {
-            return DidiFlagEventModel(
+        ): ShgFlagEventModel {
+            return ShgFlagEventModel(
                 villageId = didiEntity.villageId,
                 name = didiEntity.name,
                 guardianName = didiEntity.guardianName,
@@ -36,7 +35,42 @@ data class DidiFlagEventModel(
                 deviceId = didiEntity.localUniqueId,
                 cohortDeviceId = tolaDeviceId,
                 shgFlag = SHGFlag.fromInt(didiEntity.shgFlag).name,
-                ableBodiedFlag = AbleBodiedFlag.fromInt(didiEntity.ableBodiedFlag).name
+            )
+        }
+
+    }
+
+}
+
+data class AbleBodiedFlagEventModel(
+
+    @SerializedName("villageId") val villageId: Int,
+    @SerializedName("name") var name: String = BLANK_STRING,
+    @SerializedName("guardianName") var guardianName: String = BLANK_STRING,
+    @SerializedName("address") var address: String = BLANK_STRING,
+    @SerializedName("cohortId") var cohortId: Int = -1,
+    @SerializedName("cohortName") var cohortName: String = BLANK_STRING,
+    @SerializedName("deviceId") var deviceId: String,
+    @SerializedName("cohortDeviceId") var cohortDeviceId: String? = BLANK_STRING,
+    @SerializedName("ableBodiedFlag") var ableBodiedFlag: String
+) {
+
+    companion object {
+
+        fun getAbleBodiedFlagEventModel(
+            didiEntity: DidiEntity,
+            tolaDeviceId: String
+        ): AbleBodiedFlagEventModel {
+            return AbleBodiedFlagEventModel(
+                villageId = didiEntity.villageId,
+                name = didiEntity.name,
+                guardianName = didiEntity.guardianName,
+                address = didiEntity.address,
+                cohortId = didiEntity.cohortId,
+                cohortName = didiEntity.cohortName,
+                deviceId = didiEntity.localUniqueId,
+                cohortDeviceId = tolaDeviceId,
+                ableBodiedFlag = AbleBodiedFlag.fromInt(didiEntity.ableBodiedFlag).name,
             )
         }
 
