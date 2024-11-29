699e3acafff2f2d135808d5ac1e4854881b01241
anas <anas.mansoori@tothenew.com>
2024-11-29 14:38:23 +0530

    Code Fixes

--- core/src/main/java/com/nudge/core/ui/commonUi/CustomLinerProgressIndicator.kt
+++ core/src/main/java/com/nudge/core/ui/commonUi/CustomLinerProgressIndicator.kt
@@ -22,7 +22,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.unit.dp
 import com.nudge.core.BLANK_STRING
-import com.nudge.core.formatProgressNumber
 import com.nudge.core.ui.theme.dimen_8_dp
 import com.nudge.core.ui.theme.progressIndicatorColor
 import com.nudge.core.ui.theme.smallTextStyle
@@ -62,7 +61,7 @@ fun CustomLinearProgressIndicator(
                 .clip(RoundedCornerShape(14.dp)),
             color = color,
             trackColor = progressTrackColor,
-            progress = formatProgressNumber(progress.value),
+            progress = progress.value,
             strokeCap = strokeCap
         )
         if (showProgressText) {
