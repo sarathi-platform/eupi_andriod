d1e572cdcdbeb979948a4dd3f2c4fcd1514c0f40
ritiksingh-tothenew <114281671+ritiksingh-tothenew@users.noreply.github.com>
2024-12-18 16:01:51 +0530

    SARATHI-3903 Prod | WB | Bank account number is coming encrypted for some didis (#504)
    * SARATHI-3903 Prod | WB | Bank account number is coming encrypted for some didis
    
    * SARATHI-3903 Remove unused delay

--- surveymanager/src/main/java/com/sarathi/surveymanager/ui/screen/FormQuestionScreen.kt
+++ surveymanager/src/main/java/com/sarathi/surveymanager/ui/screen/FormQuestionScreen.kt
@@ -42,6 +42,7 @@ import androidx.compose.ui.window.DialogProperties
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.nudge.core.BLANK_STRING
+import com.nudge.core.CoreDispatchers
 import com.nudge.core.ui.commonUi.AlertDialogComponent
 import com.nudge.core.ui.commonUi.CustomVerticalSpacer
 import com.nudge.core.ui.commonUi.SubmitButtonBottomUi
@@ -91,7 +92,6 @@ import com.sarathi.surveymanager.ui.description_component.presentation.ModelBott
 import com.sarathi.surveymanager.utils.DescriptionContentState
 import com.sarathi.surveymanager.utils.getMaxInputLength
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -241,9 +241,11 @@ fun FormQuestionScreen(
                         .fillMaxWidth()
                 ) {
                     SubmitButtonBottomUi(
-                        isButtonActive = viewModel.isButtonEnable.value && viewModel.isActivityNotCompleted.value,
+                        isButtonActive = viewModel.isButtonEnable.value && viewModel.isActivityNotCompleted.value && !viewModel.isSubmitButtonClicked.value,
                         buttonTitle = stringResource(R.string.submit),
                         onSubmitButtonClick = {
+                            if (!viewModel.isSubmitButtonClicked.value) {
+                                viewModel.isSubmitButtonClicked.value = true
                             viewModel.saveAllAnswers {
                                 viewModel.updateTaskStatus(taskId)
                                 viewModel.updateSectionStatus(
@@ -253,10 +255,12 @@ fun FormQuestionScreen(
                                     taskId,
                                     SurveyStatusEnum.INPROGRESS.name
                                 )
-                                withContext(Dispatchers.Main) {
-                                    viewModel.onEvent(LoaderEvent.UpdateLoaderState(false))
+                                withContext(CoreDispatchers.mainDispatcher) {
                                     onNavigateBack()
+                                    viewModel.onEvent(LoaderEvent.UpdateLoaderState(false))
+
                                 }
+                            }
                             }
                         }
                     )
