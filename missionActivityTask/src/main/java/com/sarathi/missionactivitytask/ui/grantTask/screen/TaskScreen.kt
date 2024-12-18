d9413c7ff353cb5e826669797a93eba6dbe8cd76
ankit <ankit.jain3@tothenew.com>
2024-12-18 15:52:08 +0530

    code cleanup

--- missionActivityTask/src/main/java/com/sarathi/missionactivitytask/ui/grantTask/screen/TaskScreen.kt
+++ missionActivityTask/src/main/java/com/sarathi/missionactivitytask/ui/grantTask/screen/TaskScreen.kt
@@ -65,12 +65,10 @@ import com.nudge.core.ui.commonUi.rememberCustomBottomSheetScaffoldProperties
 import com.nudge.core.ui.theme.blueDark
 import com.nudge.core.ui.theme.defaultTextStyle
 import com.nudge.core.ui.theme.dimen_10_dp
-import com.nudge.core.ui.theme.dimen_150_dp
 import com.nudge.core.ui.theme.dimen_16_dp
 import com.nudge.core.ui.theme.dimen_16_sp
 import com.nudge.core.ui.theme.dimen_180_dp
 import com.nudge.core.ui.theme.dimen_20_dp
-import com.nudge.core.ui.theme.dimen_250_dp
 import com.nudge.core.ui.theme.dimen_48_dp
 import com.nudge.core.ui.theme.dimen_50_dp
 import com.nudge.core.ui.theme.dimen_6_dp
@@ -160,8 +158,12 @@ fun TaskScreen(
     }
 
     LaunchedEffect(viewModel.isButtonEnable.value) {
-        if (viewModel.isButtonEnable.value) {
-            scaffoldState.show()
+        viewModel.isButtonEnable.value?.let { isEnabled ->
+            if (isEnabled) {
+                scaffoldState.show()
+            } else {
+                scaffoldState.hide()
+            }
         }
     }
 
