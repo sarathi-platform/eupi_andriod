2bf3b073ef4e019bb8a4100a4dcacec6dc5479b3
aayushi <aayushi.agarwal@tothenew.com>
2024-12-31 17:44:14 +0530

    COMMENT resolve

--- core/src/main/java/com/nudge/core/helper/TranslationEnum.kt
+++ core/src/main/java/com/nudge/core/helper/TranslationEnum.kt
@@ -55,16 +55,34 @@ enum class TranslationEnum(val screenName: String, val keys: List<String> = empt
         "EditHistoryRow",
         listOf("delete", "event", "asset_type", "increse_in_number", "event_date")
     ),
-    DidiTabScreen("DidiTabScreen", listOf("are_you_sure" ,
-            "do_you_want_to_exit_the_app" ,"exit","cancel",
-        "refresh_failed_please_try_again","app_name","not_able_to_load",
-        "no_didi_s_assigned_to_you","search_didi","search_by_small_groups",
-        "total_didis")),
+    DidiTabScreen(
+        "DidiTabScreen", listOf(
+            "are_you_sure",
+            "do_you_want_to_exit_the_app", "exit", "cancel",
+            "refresh_failed_please_try_again", "app_name", "not_able_to_load",
+            "no_didi_s_assigned_to_you", "search_didi", "search_by_small_groups",
+            "total_didis"
+        )
+    ),
 
-    SmallGroupAttendanceScreen("SmallGroupAttendanceScreen", listOf("confirmation_alert_dialog_title","do_you_want_mark_all_absent","yes","no","search_didi","all")),
+    SmallGroupAttendanceScreen(
+        "SmallGroupAttendanceScreen",
+        listOf(
+            "confirmation_alert_dialog_title",
+            "do_you_want_mark_all_absent",
+            "yes",
+            "no",
+            "search_didi",
+            "all"
+        )
+    ),
 
-    SmallGroupAttendanceEditScreen("SmallGroupAttendanceEditScreen", listOf("confirmation_alert_dialog_title","delete_attendance_confirmation_msg",
-        "yes","no","submit","data_change_not_allow","ok","all")),
+    SmallGroupAttendanceEditScreen(
+        "SmallGroupAttendanceEditScreen", listOf(
+            "confirmation_alert_dialog_title", "delete_attendance_confirmation_msg",
+            "yes", "no", "submit", "data_change_not_allow", "ok", "all"
+        )
+    ),
 
     SmallGroupAttendanceHistoryScreen(
         "SmallGroupAttendanceHistoryScreen",
@@ -75,14 +93,13 @@ enum class TranslationEnum(val screenName: String, val keys: List<String> = empt
             "no",
             "take_attendance_button_text",
             "date_range_picker_label_text",
-            "date_range_picker_label_text",
             "attendance_history_header_text",
             "total_didis_label_text",
-            "take_attendance_button_text",
             "attendance_percentage_text",
             "total_count_text",
             "edit_button_text",
             "delete_button_text",
+            "present",
             "absent"
         )
     ),
