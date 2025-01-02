8c1420ecc69ab5a1880388c7c77ae8430634b619
ritiksingh-tothenew <114281671+ritiksingh-tothenew@users.noreply.github.com>
2025-01-02 11:01:17 +0530

    SARATHI-4008 Fixed No didi's assigned. But in the Didi tab its showing not able to load data msg instead of no didi's assigned. (#523)

--- core/src/main/java/com/nudge/core/database/entities/ApiStatusEntity.kt
+++ core/src/main/java/com/nudge/core/database/entities/ApiStatusEntity.kt
@@ -5,7 +5,9 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import androidx.room.TypeConverters
 import com.nudge.core.ApiStatusTable
+import com.nudge.core.BLANK_STRING
 import com.nudge.core.database.converters.DateConverter
+import com.nudge.core.enums.ApiStatus
 import com.nudge.core.toDate
 import java.util.Date
 
@@ -32,6 +34,18 @@ data class ApiStatusEntity(
 
     @ColumnInfo("error_code")
     val errorCode: Int
-)
+) {
+    companion object {
+        fun getApiStatusEntity(apiEndPoint: String) =
+            ApiStatusEntity(
+                apiEndpoint = apiEndPoint,
+                status = ApiStatus.INPROGRESS.ordinal,
+                modifiedDate = System.currentTimeMillis().toDate(),
+                createdDate = System.currentTimeMillis().toDate(),
+                errorCode = 0,
+                errorMessage = BLANK_STRING
+            )
+    }
+}
 
 
