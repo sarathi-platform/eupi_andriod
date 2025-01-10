bf75697c2e85ad08040a21c260c048864171f16a
ankit <ankit.jain3@tothenew.com>
2025-01-10 16:34:53 +0530

    Add Livelihood Planning translations for WB,AS and TRI

--- surveymanager/src/main/java/com/sarathi/surveymanager/ui/screen/livelihood/LivelihoodPlaningViewModel.kt
+++ surveymanager/src/main/java/com/sarathi/surveymanager/ui/screen/livelihood/LivelihoodPlaningViewModel.kt
@@ -7,6 +7,7 @@ import com.nudge.core.DEFAULT_ID
 import com.nudge.core.DEFAULT_LIVELIHOOD_ID
 import com.nudge.core.DIDI
 import com.nudge.core.LIVELIHOOD
+import com.nudge.core.helper.TranslationEnum
 import com.nudge.core.preference.CoreSharedPrefs
 import com.nudge.core.ui.events.DialogEvents
 import com.nudge.core.utils.CoreLogger
@@ -71,6 +72,7 @@ class LivelihoodPlaningViewModel @Inject constructor(
             }
 
             is InitDataEvent.InitDataState -> {
+                setTranslationConfig()
                 initLivelihoodPlanningScreen()
             }
 
@@ -222,5 +224,9 @@ class LivelihoodPlaningViewModel @Inject constructor(
             )
         }
         callBack()
+    }
+
+    override fun getScreenName(): TranslationEnum {
+        return TranslationEnum.LivelihoodDropDownScreen
     }
 }
