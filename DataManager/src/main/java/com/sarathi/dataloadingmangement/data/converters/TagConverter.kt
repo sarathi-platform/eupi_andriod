5c8de1e78772bd90d27f0f20fd4f046f87ab2f4c
Atul Mishra <atul.mishra@tothenew.com>
2025-02-13 09:44:59 +0530

    Tag empty crash resolved

--- DataManager/src/main/java/com/sarathi/dataloadingmangement/data/converters/TagConverter.kt
+++ DataManager/src/main/java/com/sarathi/dataloadingmangement/data/converters/TagConverter.kt
@@ -1,12 +1,15 @@
 package com.sarathi.dataloadingmangement.data.converters
 
+import android.text.TextUtils
+import androidx.compose.ui.unit.TextUnit
 import androidx.room.TypeConverter
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import com.nudge.core.BLANK_STRING
 
 class TagConverter {
     @TypeConverter
-    fun fromIntListToJson(intList: List<Int>?): String? {
+    fun fromIntListToJson(intList: List<Int?>?): String? {
         if (intList == null) {
             return null
         }
@@ -18,8 +21,8 @@ class TagConverter {
 
     @TypeConverter
     fun fromJsonToIntList(intList: String?): List<Int>? {
-        if (intList == null) {
-            return null
+        if (TextUtils.isEmpty(intList)) {
+            return emptyList()
         }
         val gson = Gson()
         val type = object :
