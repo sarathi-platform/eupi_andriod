169df63733fbb3eadd89af70b0f6a66584432392
ritik <ritik.singh@tothenew.com>
2025-03-19 12:33:54 +0530

    Fixed security audit issues

--- app/src/main/java/com/patsurvey/nudge/network/model/ErrorDataModel.kt
+++
@@ -1,8 +0,0 @@
-package com.patsurvey.nudge.network.model
-
-import com.google.gson.annotations.SerializedName
-
-data class ErrorDataModel(
-    @SerializedName("errorCode") val errorCode: Int = -1,
-    @SerializedName("message") val message: String? = null,
-)
